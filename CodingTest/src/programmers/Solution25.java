a594b530e33a238b665570e2c8fde71482993155
Jeeho Kim <rlawlgh3245@gmail.com>
2022-09-14 07:21:45 +0900

    09/14 07:21

---
+++ CodingTest/src/programmers/Solution25.java
@@ -0,0 +1,21 @@
+package programmers;
+
+public class Solution25 {
+	class Solution {
+		public int solution(String dartResult) {
+			int answer = 0;
+			
+			String[] str=new String[dartResult.length()];
+
+			for (int i = 0; i < dartResult.length(); i++) {
+				str[i]=String.valueOf(dartResult.charAt(i));
+			}
+			
+			
+
+			return answer;
+
+		}
+	}
+
+}
--- CodingTest/src/programmers/Solution6.java
+++ CodingTest/src/programmers/Solution6.java
@@ -6,23 +6,25 @@ public class Solution6 {
 	class Solution {
 		public int solution(int[][] board, int[] moves) {
 			int answer = 0;
-			Stack<Integer> s=new Stack<Integer>();
-					
+			Stack<Integer> s = new Stack<Integer>();
 
 			for (int i = 0; i < moves.length; i++) {
 				for (int j = 0; j < board.length; j++) {
 					if (board[j][moves[i] - 1] != 0) {
-						if(s.isEmpty()) {
+						if (s.isEmpty()) {
 							s.push(board[j][moves[i] - 1]);
-						}
-						if(board[j][moves[i] - 1]==s.peek()) {
-							answer+=2;
-						}
-						else {
-							s.push(board[j][moves[i] - 1]);
+						} else {
+							if (board[j][moves[i] - 1] == s.peek()) {
+								s.pop();
+								answer += 2;
+							} else {
+								s.push(board[j][moves[i] - 1]);
+							}
+
 						}
 						board[j][moves[i] - 1] = 0;
 						break;
+
 					}
 				}
 			}
