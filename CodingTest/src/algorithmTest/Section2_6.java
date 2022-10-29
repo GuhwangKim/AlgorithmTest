154e8bcdd7f15e5ba8f441a9df00330dc20b67da
Jeeho Kim <rlawlgh3245@gmail.com>
2022-10-30 00:41:57 +0900

    10/30

---
+++ CodingTest/src/algorithmTest/Section2_6.java
@@ -0,0 +1,50 @@
+package algorithmTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Section2_6 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int inp = in.nextInt();
+		String[] arr1 = new String[inp];
+		String[] arr2 = new String[inp];
+		List<Integer> list = new ArrayList<Integer>();
+		
+		for (int i = 0; i < inp; i++) {
+			arr1[i] = in.next();
+		}
+		for (int i = 0; i < inp; i++) {
+			for (int j = arr1[i].length()-1; j <=0 ; j++) {
+				arr2[i]+=arr1[i].charAt(j);
+			}
+		}
+		for(String str : arr2) {
+			System.out.println(str);
+		}
+		for (int i = 0; i < inp; i++) {
+			int num = Integer.parseInt(arr2[i]);
+			if(num==2) {
+				list.add(num);
+				break;
+			}
+			boolean chk = true;
+			for (int j = 2; j <num; j++) {
+				if(num%j==0) {
+					chk = false;
+					break;
+				}
+			}
+			if(chk==true) {
+				list.add(num);
+			}
+			
+		}
+		for(int x : list) {
+			System.out.println(x);
+		}
+		
+		return;
+	}
+}
