5d931a4d2297b0b61c8aef09c4f4db5bcb6225f0
Jeeho Kim <rlawlgh3245@gmail.com>
2022-11-06 22:51:00 +0900

    11/06

--- CodingTest/src/algorithmTest/Section2_12.java
+++ CodingTest/src/algorithmTest/Section2_12.java
@@ -3,34 +3,47 @@ package algorithmTest;
 import java.util.Scanner;
 
 public class Section2_12 {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String result = in.nextLine();
-		String[] arr = result.split(" ");
-		int tot = Integer.parseInt(arr[0]);
-		int row = Integer.parseInt(arr[1]);
-		String[] sarr = new String[row];
-		
-		for (int i = 0; i < row; i++) {
-			sarr[i]=in.nextLine();
-		}
-		int[] iarr = new int[tot];
-		for (int i = 0; i < row; i++) { // 3번 행개수 
-			String[] tarr = sarr[i].split(" ");
-			for (int j = 0; j < tot; j++) { // 4번 행의 열 개수 
-				int var = Integer.parseInt(tarr[j]);
-				if(i==0) {
-					iarr[var-1] = tot-j-1;
-				}else if (iarr[var-1]>tot-j-1) {
-					iarr[var-1] = tot-j-1;
+	public int solution(int n, int m, int[][] arr) {
+		int answer = 0;
+		for (int i = 1; i <=n; i++) { // 멘토
+			for (int j = 1; j <=n; j++) { // 멘티 
+				int cnt = 0; // 새로운 조합
+				for (int k = 0; k < m; k++) { // 횟수
+					int pi =0, pj=0;
+					for (int s = 0; s < n; s++) { // 등수 (한줄)
+						if(arr[k][s]==i) { // 멘토인경우
+							pi=s; // 등수 
+						}
+						if(arr[k][s]==j) { // 멘티인경우
+							pj=s; 
+						}
+					}
+					// 한줄 끝났음 
+					if(pi<pj) {
+						cnt++;
+					}
+					// 다음 줄 
+				}
+				if(cnt==m) {
+					answer++;
 				}
 			}
 		}
-		int sum=0;
-		for(int x : iarr) {
-			sum+=x;
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Section2_12 T = new Section2_12();
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int[][] arr = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j]=in.nextInt();
+			}
 		}
-		System.out.println(sum);
+		System.out.println(T.solution(n, m, arr));
 	}
 
 }
