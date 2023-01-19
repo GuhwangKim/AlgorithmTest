d6b8ea52d85c7ee6c12297e7c4d1dff76ddfa801
Jeeho Kim <rlawlgh3245@gmail.com>
2023-01-19 21:28:50 +0900

    01/19

--- CodingTest/src/algorithmTest/Section8_1.java
+++ CodingTest/src/algorithmTest/Section8_1.java
@@ -3,32 +3,45 @@ package algorithmTest;
 import java.util.Scanner;
 
 public class Section8_1 {
-	Node root;
-	public void DFS(Node root) {
-		if(root==null) return; 
-		else {
-			DFS(root.lt);
-			System.out.print(root.data+" ");
-			DFS(root.rt);
+	static int[] ch;
+	static int[] arr;
+	static String answer = "NO";
+	static int num = 0;
+	static int total;
+	int sum = 0;
+	
+	private void DFS(int i) {
+		sum +=arr[i];
+		while (num<arr.length) {
+			if(sum==(total-sum)) {
+				answer = "YES";
+				break;
+			} else {
+				for (int j = 1; j <= arr.length; j++) {
+					if(ch[j]==0) {
+						ch[j]=1;
+						DFS(j);
+						ch[j]=0;
+						sum-=arr[j];
+					}
+				}
+			}
 		}
+		System.out.println(answer);
 	}
 	
-	
 	public static void main(String[] args) {
-		Section8_1 tree = new Section8_1();
-		tree.root = new Node(1);
-		tree.root.lt = new Node(2);
-		tree.root.rt = new Node(3);
-		tree.root.lt.lt = new Node(4);
-		tree.root.lt.rt = new Node(5);
-		
+		Section8_1 T = new Section8_1();
 		Scanner in = new Scanner(System.in);
 		int a = in.nextInt();
-		int[] arr = new int[a];
+		arr = new int[a];
 		for (int i = 0; i < arr.length; i++) {
 			arr[i]=in.nextInt();
+			total+=arr[i];
 		}
-		
+		ch = new int[a+1];
+		ch[1]=1;
+		T.DFS(0);
 	}
 
 }
