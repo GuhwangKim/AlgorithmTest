89783f212e4d44013b100e6c6f435fbff636e28c
Jeeho Kim <rlawlgh3245@gmail.com>
2023-02-25 01:11:26 +0900

    23/02/25

---
+++ CodingTest/src/algorithmTest/Section8_8.java
@@ -0,0 +1,16 @@
+package algorithmTest;
+
+import java.util.Scanner;
+
+public class Section8_8 {
+	
+	
+	
+	public static void main(String[] args){
+	    Scanner in=new Scanner(System.in);
+	    int count = in.nextInt();
+	    int bottom = in.nextInt();
+	    return ;
+	  }
+
+}
