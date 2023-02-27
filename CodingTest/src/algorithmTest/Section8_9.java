2ff05172ee899f3b30a8270c1e7b7e4a1d424a3c
JeehoKim <rlawlgh3245@gmail.com>
2023-02-27 23:35:59 +0900

    23/02/27

---
+++ CodingTest/src/algorithmTest/Section8_9.java
@@ -0,0 +1,31 @@
+package CodingTest.src.algorithmTest;
+
+import java.util.Scanner;
+
+public class Section8_9 {
+    static int[] combi;
+    static int n,m;
+
+    private void DFS(int L, int s) {
+        if(L == m){
+            for (int x : combi){
+                System.out.println(x+" ");
+                System.out.println();
+            }
+        } else{
+            for (int i = s; i <= n; i++) { // 뻗어나감
+                combi[L]=i;
+                DFS(L+1, i+1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Section8_9 T = new Section8_9();
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt(); // 4
+        m = in.nextInt(); //2
+        combi= new int[m];
+        T.DFS(0,1);
+    }
+
+}
