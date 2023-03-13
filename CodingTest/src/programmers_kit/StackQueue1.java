47f3692ae91d535a6fd6ed096b596a1b7f195407
Jeeho Kim <rlawlgh3245@gmail.com>
2023-03-13 23:25:11 +0900

    23/03/13

---
+++ CodingTest/src/programmers_kit/StackQueue1.java
@@ -0,0 +1,37 @@
+package programmers_kit;
+
+import java.util.Stack;
+
+
+public class StackQueue1 {
+
+	public int[] solution(int[] arr) {
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < arr.length; i++) {
+			if(stack.isEmpty()||stack.peek()!=arr[i]) {
+				stack.add(arr[i]);
+			}
+		}
+		int[] answer = new int[stack.size()];
+		
+		for (int i = answer.length-1 ; i >= 0; i--) {
+			answer[i]=stack.pop();
+		}
+		return answer;
+	}
+		
+
+	
+	public static void main(String[] args) {
+		StackQueue1 t = new StackQueue1();
+		int[] arr = new int[5];
+		arr[0] = 4;
+		arr[1] = 4;
+		arr[2] = 4;
+		arr[3] = 3;
+		arr[4] = 3;
+		for(int x : t.solution(arr)) {
+			System.out.print(x+" ");
+		}
+	}
+}
