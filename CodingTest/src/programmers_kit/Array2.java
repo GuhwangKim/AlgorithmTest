c846e461affd96770345b81c68f7905342edb5da
Jeeho Kim <rlawlgh3245@gmail.com>
2023-04-25 00:36:32 +0900

    23/04/24

---
+++ CodingTest/src/programmers_kit/Array2.java
@@ -0,0 +1,13 @@
+package programmers_kit;
+
+public class Array2 {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+		/*
+		 * 1. 모든 자리수를 1000단위로 맞춤 
+		 * 2. 순서를 매김 
+		 */
+        return answer;
+    }
+}
