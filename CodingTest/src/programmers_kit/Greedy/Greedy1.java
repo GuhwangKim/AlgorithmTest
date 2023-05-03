475df02a6055cb4e13b453325737f7bb1424d6e1
Jeeho Kim <rlawlgh3245@gmail.com>
2023-05-04 00:22:52 +0900

    23/05/04

---
+++ CodingTest/src/programmers_kit/Greedy/Greedy1.java
@@ -0,0 +1,13 @@
+package programmers_kit.Greedy;
+
+public class Greedy1 {
+	public int solution(String name) {
+        int answer = 0;
+        
+        // 1) 위 아래로 움직임 원리 : 해당 글자의 - A / 전체에서 (해당글 - A) 더 짧은 쪽으로 올라감 
+        // 2) 인덱스 안의 값 중에, 그 다음값이 A 인 경우 
+        
+        return answer;
+    }
+
+}
