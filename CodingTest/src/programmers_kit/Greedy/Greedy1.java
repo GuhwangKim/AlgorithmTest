638cde9be7365d1826f2c7c72f520b01994f16e6
Jeeho Kim <rlawlgh3245@gmail.com>
2023-05-05 01:22:14 +0900

    23/05/04

--- CodingTest/src/programmers_kit/Greedy/Greedy1.java
+++ CodingTest/src/programmers_kit/Greedy/Greedy1.java
@@ -3,9 +3,24 @@ package programmers_kit.Greedy;
 public class Greedy1 {
 	public int solution(String name) {
         int answer = 0;
+        int move = name.length()-1;
         
         // 1) 위 아래로 움직임 원리 : 해당 글자의 - A / 전체에서 (해당글 - A) 더 짧은 쪽으로 올라감 
         // 2) 인덱스 안의 값 중에, 그 다음값이 A 인 경우 
+        
+        for (int i = 0; i < name.length(); i++) {
+			answer+=Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i));
+			
+			// 그 다음수가 A 라면 A의 마지막 위치 찾음 
+			int idxNow = i + 1;
+			while(idxNow<name.length()&&name.charAt(idxNow)=='A') {
+				idxNow++;
+			}
+			
+			
+			
+		}
+        
         
         return answer;
     }
