ab1e9b2d73a563e436de440f511ecbc1be351bbd
Jeeho Kim <rlawlgh3245@gmail.com>
2023-05-16 23:24:07 +0900

    23/05/16

--- CodingTest/src/programmers_kit/StackQueue/StackQueue3.java
+++ CodingTest/src/programmers_kit/StackQueue/StackQueue3.java
@@ -1,4 +1,4 @@
-package CodingTest.src.programmers_kit.StackQueue;
+package programmers_kit.StackQueue;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -6,36 +6,44 @@ import java.util.List;
 import java.util.Queue;
 
 public class StackQueue3 {
-    public List<Integer> solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
-        /*
-        7, 3, 9
-        5, 10, 1, 1, 20, 1
 
-        */
-        Queue<Integer> queue = new LinkedList<>();
+	public static void main(String[] args) {
+		StackQueue3 h = new StackQueue3();
+		int[] progresses = { 93, 30, 55 };
+		int[] speeds = { 1, 30, 5 };
+		h.solution(progresses, speeds);
+	}
 
-        for (int i = 0; i < progresses.length; i++) {
-            for (int j = 1; j <=100 ; j++) {
-                progresses[0]+=speeds[0];
-                if(progresses[0]==100) {
-                    queue.add(j);
-                    break;
-                }
-            }
-        }
+	public List<Integer> solution(int[] progresses, int[] speeds) {
+		List<Integer> answer = new ArrayList<>();
+		/*
+		 * 7, 3, 9 5, 10, 1, 1, 20, 1
+		 * 
+		 */
+		Queue<Integer> queue = new LinkedList<>();
 
-        while (queue.isEmpty()){
-            int cnt = 1;
-            int tot = 0;
-            int queueFirst = queue.poll();
-            if(tot>queue.peek()){
-                tot += queueFirst;
-                cnt++;
-            }else{
-               answer.add(cnt);
-            }
-        }
-        return answer;
-    }
+		 for (int i = 0; i < progresses.length; i++) {
+	            if ((100 - progresses[i]) % speeds[i] == 0) {
+	            	queue.add((100 - progresses[i]) / speeds[i]);
+	            } else {
+	            	queue.add((100 - progresses[i]) / speeds[i] + 1);
+	            }
+	        }
+
+		int cnt = 1;
+		int compare = queue.poll();
+		while (!queue.isEmpty()) {
+			if (compare>=queue.peek()) {
+				cnt++;
+				queue.poll();
+			}else {
+				answer.add(cnt);
+				cnt = 1;
+				compare = queue.poll();
+			}
+			
+		}
+		answer.add(cnt);
+		return answer;
+	}
 }
