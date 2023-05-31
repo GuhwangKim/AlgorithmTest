3299ea29fea66f0d1f5ae1cefbb49fedca277f93
Jeeho Kim <rlawlgh3245@gmail.com>
2023-05-31 21:58:18 +0900

    23/05/31

--- CodingTest/src/programmers_kit/Search/BinarySearch1.java
+++ CodingTest/src/programmers_kit/Search/BinarySearch1.java
@@ -1,69 +1,40 @@
-package CodingTest.src.programmers_kit.Search;
+package programmers_kit.Search;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.Arrays;
 
 public class BinarySearch1 {
-    static  long totalTimes = 0;
-    public long solution(int n, int[] times) {
-
-        LinkedList<Integer>list = new LinkedList<>();
-
-        for (int i = 0; i < times.length; i++) {
-            // n/2+1 의 길이 만큼만 queue 에 그 배수들을 집어 넣어두고 싶음
-            for (int j = 1; j <= (n/times.length)+1; j++) {
-                list.add(times[i]*j);
-            }
+	public long solution(int n, int[] times) {
+        long answer = 0;
+        
+        // 1. times에 있는 숫자를 정렬 <- 이분탐색은 정렬이 되어야만함 
+        Arrays.sort(times);
+        // 2. times 의 최소값과 최대값을 구함 <- 가운데 숫자를 구하기 위함
+        long maxTime = times[times.length-1]*(long)n;
+        long minTime = 0;
+        
+        // 3. minTime >= maxTime 이 될때까지 계속해서 중간값으로 범위를 줄이기 
+        while(minTime<=maxTime) {
+        	long sumPeople = 0;
+        	// 4. 중간값을 구함 
+        	long mid = (maxTime+minTime)/2;
+        	// 5. times 배열에 있는 각 값을 mid 로 나눈 값, 즉 처리할 수 있는 인원의 수를 더함 
+        	for(long value : times) {
+        		sumPeople+=(mid/value);
+        	}
+        	// 6. 총 처리할 수 있는 인원의 수가 주어진 n 보다 큰 경우 
+        	// = mid 값이면 이미 일처리 다 끝났음 => mid 값을 더 줄일 수 있음 
+        	if(sumPeople>=n) {
+        		answer = mid;
+        		// = 오른쪽 값들을 전부 다 버림 
+        		maxTime = mid-1;
+        	} else {
+        		// 7. 인원을 다 처리하지 못 함
+        		// = mid 값을 더 늘려야함 
+        		minTime = mid+1;
+        	}
         }
-        // 정렬
-        list.sort((a1, a2)->a1-a2);
-        // n번째
-        totalTimes = list.get(n-1);
-
-        /*Map<Integer, Queue<Integer>> map = new HashMap<>();
-
-        if(n==0){
-            // 현재 기다리는 사람이 없다면 반환
-            return totalTimes;
-        }
-
-        for (int i = 0; i < times.length; i++) {
-            // n/2+1 의 길이 만큼만 queue 에 그 배수들을 집어 넣어두고 싶음
-            for (int j = 1; j <= (n/times.length)+1; j++) {
-                Queue<Integer> queue = new LinkedList<>();
-                queue.add(times[i]*j);
-                map.put(i,queue);
-            }
-        }
-
-        DFS(map, n);*/
-
-
-        return totalTimes;
+        
+        return answer;
     }
 
-/*    private void DFS(Map<Integer, Queue<Integer>> timesValues, int people) {
-        if(people==0){
-            return;
-        }
-
-        for (int i = 0; i <= timesValues.size(); i++) {
-            // map 에 담겨 있는 각각의 숫자들을 비교하고 가장 적은 것들을 뽑아냄
-            // n 을 줄어들게 하고 n 이 0이라면 스탑
-            int preNum = timesValues.get(i).peek();
-            int nextNum = timesValues.get(i+1).peek();
-            if(preNum<nextNum){
-                totalTimes+=preNum;
-                // 제거 함 한 사람 끝남
-                timesValues.get(i).poll();
-                DFS(timesValues, people-1);
-            }else{
-
-            }
-        }
-
-
-    }*/
 }
