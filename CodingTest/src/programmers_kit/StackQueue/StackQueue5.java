322e11e217fa8ca23156fbc0799ac03cff180c99
Jeeho Kim <rlawlgh3245@gmail.com>
2023-06-01 21:26:12 +0900

    23/06/01

---
+++ CodingTest/src/programmers_kit/StackQueue/StackQueue5.java
@@ -0,0 +1,20 @@
+package programmers_kit.StackQueue;
+
+public class StackQueue5 {
+	public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+       for (int i = 0; i < prices.length; i++) {
+           for (int j = i+1; j < prices.length; j++) {
+        	   answer[i]++;
+               if(prices[i]>prices[j]){
+                   break;
+               }
+           }
+           
+       }
+       
+       return answer;
+   }
+
+}
