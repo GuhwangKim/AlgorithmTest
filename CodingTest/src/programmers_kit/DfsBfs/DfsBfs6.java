f49f515e249393316fa41c303d209a368cf63112
JeehoKim <rlawlgh3245@gmail.com>
2023-06-09 13:10:59 +0900

    23/06/09

--- CodingTest/src/programmers_kit/DfsBfs/DfsBfs6.java
+++ CodingTest/src/programmers_kit/DfsBfs/DfsBfs6.java
@@ -1,32 +1,42 @@
 package CodingTest.src.programmers_kit.DfsBfs;
 
 public class DfsBfs6 {
-    public int solution(int[][] game_board, int[][] table) {
+    int[] parent;
+    public int solution(int n, int[][] computers) {
         int answer = 0;
-        for (int i = 0; i < game_board.length; i++) {
-            for (int j = 0; j < game_board.length; j++) {
-                //1. 게임보드 숫자를 테이블이랑 똑같이 만들어주기
-                game_board[i][j] = game_board[i][j]==0?1:0;
-            }
-        }
-        for (int i = 0; i < game_board.length; i++) {
-            for (int j = 0; j < game_board.length; j++) {
-                //1. 게임보드 숫자를 테이블이랑 똑같이 만들어주기
-                if(game_board[i][j]==1){
-                    String blockString = bfs(game_board, i, j);
-                }
-            }
+        /**
+         * 1. arr[i][j]가 1인 경우 -> 그 반대도 1이 되*/
+
+        parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
 
-
+        for (int startNum = 0; startNum < n; startNum++) {
+            for (int endNum = 0; endNum < n; endNum++) {
+               if(computers[startNum][endNum]==1){
+                   // 서로 연결된 경우 -> 하위 j의 상위
+                   int startNumsParent = findParentNum(startNum);
+                   int endsNumParent = findParentNum(endNum);
+                   if(endsNumParent==startNumsParent){
+                       // 두개의 부모가 같다면
+                   } else{
+                       // 두개의 부모가 다르다면 하위의 부모 값을 상위로 변경
+                       parent[endsNumParent]=startNum;
+                   }
+               }
+            }
+        }
         return answer;
     }
 
-
-
-    String bfs(int[][] board, int x, int y){
-        int dx[] = new int[] {1, -1, 0, 0};
-        int dy[] = new int[] {0, 0, 1, -1};
-
+    private int findParentNum(int nodeNum) {
+        if(parent[nodeNum]==nodeNum){
+            return nodeNum;
+        } else{
+            return parent[nodeNum] =
+        }
+        return 1;
     }
 }
