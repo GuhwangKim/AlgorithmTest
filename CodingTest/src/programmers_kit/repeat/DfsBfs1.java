3501dc7794a78ff6c0a9a4df893412c77cc45582
Jeeho Kim <rlawlgh3245@gmail.com>
2023-06-21 20:03:29 +0900

    23/06/21

--- CodingTest/src/programmers_kit/repeat/DfsBfs1.java
+++ CodingTest/src/programmers_kit/repeat/DfsBfs1.java
@@ -1,5 +1,10 @@
 package CodingTest.src.programmers_kit.repeat;
 
+/**
+ * 1. BFS 에 level을 하나씩 더해주고 
+ * 2. 더해준 level을 배열의 index로 받을 것이기 때문에 for 문 loop가 필요 없음 
+ * */
+
 public class DfsBfs1 {
     int cnt;
     public int solution(int[] numbers, int target) {
@@ -9,12 +14,14 @@ public class DfsBfs1 {
     }
 
     private void BFS(int[] numbers, int target, int sum, int level) {
-        if(level==numbers.length && sum==target){
+        if(level==numbers.length){
+        	if(sum==target)
             cnt++;
+            return;
 
         }
 
-        BFS(numbers, target, sum+numbers[level+1], level+1);
-        BFS(numbers, target, sum-numbers[level+1], level+1);
+        BFS(numbers, target, sum+numbers[level], level+1);
+        BFS(numbers, target, sum-numbers[level], level+1);
     }
 }
