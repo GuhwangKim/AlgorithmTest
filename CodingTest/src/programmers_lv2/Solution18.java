ff355563016e2f069362283d6cc7f6759090be62
Jeeho Kim <rlawlgh3245@gmail.com>
2023-07-08 21:39:49 +0900

    23/07/08

--- CodingTest/src/programmers_lv2/Solution18.java
+++ CodingTest/src/programmers_lv2/Solution18.java
@@ -1,79 +1,46 @@
-package CodingTest.src.programmers_lv2;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+
+package programmers_lv2;
+
+import java.util.Stack;
 
 public class Solution18 {
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
-    LinkedList<Integer> answer = new LinkedList<>();
-    boolean[][] visited;
+	public int[] solution(int[] numbers) {
+		int[] answer = new int[numbers.length];
+		
+		
+		/**
+		 * stack 에 인덱스를 집어 넣는다.  
+		 * */
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for (int i = numbers.length-1; i >=0 ; i--) {
+			// 1. 거꾸로 시작함 
+			while(!stack.isEmpty()) {
+				if(stack.peek()>numbers[i]) {
+					// 4. stack 에 저장되어 있는 수가 기준이 되는 수보다 크다면 조건에 만족한 경우 
+					answer[i] = stack.peek();
+					// 5 그 때 정답으로 넣어주고 
+					break;
+				}
+				stack.pop();
+				//6 뒤에있는 수 부터 비교하기 때문에 조건에 안 맞는 값이면 지워도 됨, 
+				// 어차피 기준 값 보다 작은 값이면 앞에 있는 값의 큰 값이 기준 값이 되기 때문
+			}
+			
+			if(stack.isEmpty()) {
+				// 2. stack 이 비었다면 자기 보다 큰 수가 없다는 것 
+				// 자기보다 큰 수가 없을 때 pop()를 진행함 
+				answer[i]=-1;
+			}
+			
+			stack.add(numbers[i]);
+			// 3. 현재의 수가 기준이 됨 
+			
+		}
+		
+		return answer;
+	}
 
-    public List<Integer> solution(String[] maps) {
-        visited = new boolean[maps.length][maps[0].length()];
-        // 좌표를 넘거야 할 듯
-
-        for (int i = 0; i < maps.length; i++) { // 행
-            for (int j = 0; j < maps[0].length(); j++) { // 열
-                if (!visited[i][j] && maps[0].charAt(j) != 'X') {
-                    answer.add(BFS(maps, i, j));
-                    // 리턴 값으로 연결된 섬의 값을 반환하게 해서 바로 집어넣음
-                }
-            }
-        }
-
-
-        if (answer.size() == 0) {
-            // 아무것도 없는 경우
-            answer.add(-1);
-        }
-        Collections.sort(answer);
-
-        return answer;
-    }
-
-    private int BFS(String[] maps, int presentX, int presentY) {
-
-        int linkedTotal = 0;
-        Queue<int[]> queue = new LinkedList<>();
-
-        // 현재 좌표 x y 값을 집어넣음
-        queue.add(new int[]{presentX, presentY});
-
-        // 방문함
-        visited[presentX][presentY] = true;
-
-
-        // * 언제까지 반복 ?
-        while (!queue.isEmpty()) {
-            int currentPosition[] = queue.poll();
-            // 그때의 값을 더함
-            linkedTotal += maps[currentPosition[0]].charAt(currentPosition[1]) - '0';
-
-
-            // 좌우 상하
-            for (int i = 0; i < 4; i++) {
-
-                int nextX = currentPosition[0] + dx[i];
-                int nextY = currentPosition[1] + dy[i];
-
-                if (nextX >= maps.length || nextX < 0 || nextY >= maps[0].length() || nextY < 0) {
-                    continue;
-                    // 해당이 안되면 넘어가면 됨
-                }
-                if (!visited[nextX][nextY] && maps[nextX].charAt(nextY) != 'X') {
-                    visited[nextX][nextY] = true;
-                    queue.offer(new int[]{nextX, nextY});
-                }
-
-            }
-
-        }
-
-        return linkedTotal;
-    }
 }
-
-
