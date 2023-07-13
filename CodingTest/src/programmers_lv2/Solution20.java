bd3ed2af6cb6e838553bb9f0d7f22f77073e53d0
Jeeho Kim <rlawlgh3245@gmail.com>
2023-07-13 22:55:19 +0900

    23/07/13

---
+++ CodingTest/src/programmers_lv2/Solution20.java
@@ -0,0 +1,20 @@
+package programmers_lv2;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution20 {
+	public long solution(int[] weights) {
+		long answer = 0;
+		List<Integer> list = new LinkedList<Integer>();
+		
+		for(int weight : weights) {
+			list.add(weight);
+		}
+		
+
+		
+		return answer;
+	}
+
+}
