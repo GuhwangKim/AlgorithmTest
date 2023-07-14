9a553c9aef6e7dba4d98628b4c76b139be9556dc
JeehoKim <rlawlgh3245@gmail.com>
2023-07-14 12:43:16 +0900

    23/07/14

--- CodingTest/src/programmers_lv2/Solution20.java
+++ CodingTest/src/programmers_lv2/Solution20.java
@@ -1,20 +1,41 @@
-package programmers_lv2;
+package CodingTest.src.programmers_lv2;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Solution20 {
-	public long solution(int[] weights) {
-		long answer = 0;
-		List<Integer> list = new LinkedList<Integer>();
-		
-		for(int weight : weights) {
-			list.add(weight);
-		}
-		
+    public long solution(int[] weights) {
+        long answer = 0;
 
-		
-		return answer;
-	}
+        Arrays.sort(weights);
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        for(long weight : weights){
+            queue.add(weight);
+        }
+        HashSet<Long> set = new HashSet<>();
+        for(long weight : weights){
+            set.add(weight*1);
+            set.add(weight*2);
+            set.add(weight*3);
+            set.add(weight*4);
 
+        }
+
+        while(!queue.isEmpty()){
+            long num = queue.poll();
+            for (int i = 1; i <=4 ; i++) {
+                long standnum = num*i;
+                if(set.contains(standnum)){
+                    answer++;
+                    set.remove(standnum);
+                    continue;
+                }
+            }
+        }
+
+        return answer;
+    }
 }
