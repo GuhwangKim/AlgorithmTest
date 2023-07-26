e749966e0f4e40fd069eef66110488f4b61b5272
Jeeho Kim <rlawlgh3245@gmail.com>
2023-07-26 22:51:44 +0900

    23/07/26

--- CodingTest/src/programmers_lv2/Solution24.java
+++ CodingTest/src/programmers_lv2/Solution24.java
@@ -5,13 +5,25 @@ public class Solution24 {
         int answer = 0;
         int count = storey;
         // 가장 최대로 내려오는 방법 -1
-        int[] button = {-1, 1, -10, 10, -100, 100};
         /**
          * storey + button의 값들을 조합해서 = 0
          * 그떄의 count를 저장하고
          * 그 중 가장 최소 값을 구함
          *
          * */
+        
+        int lastNum = storey%10;
+        
+        if(lastNum>5) {
+        	answer+=(10-lastNum);
+        	
+        }else if (lastNum==5) {
+			
+        	
+		} else {
+			answer += lastNum;
+			
+		}
 
 
         return answer;
