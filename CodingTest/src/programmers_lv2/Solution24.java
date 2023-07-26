eb5f476d4ea92e7716d397c7ebaa506bee2772a1
JeehoKim <rlawlgh3245@gmail.com>
2023-07-26 13:14:42 +0900

    23/07/26

---
+++ CodingTest/src/programmers_lv2/Solution24.java
@@ -0,0 +1,19 @@
+package CodingTest.src.programmers_lv2;
+
+public class Solution24 {
+    public int solution(int storey) {
+        int answer = 0;
+        int count = storey;
+        // 가장 최대로 내려오는 방법 -1
+        int[] button = {-1, 1, -10, 10, -100, 100};
+        /**
+         * storey + button의 값들을 조합해서 = 0
+         * 그떄의 count를 저장하고
+         * 그 중 가장 최소 값을 구함
+         *
+         * */
+
+
+        return answer;
+    }
+}
