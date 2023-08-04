71bc885235e2b6587e1c17e770edb8dc21adfb02
Jeeho Kim <rlawlgh3245@gmail.com>
2023-08-05 00:05:01 +0900

    23/08/04

--- CodingTest/src/programmers_kit/repeat/Greedy3.java
+++ CodingTest/src/programmers_kit/repeat/Greedy3.java
@@ -1,27 +1,24 @@
 package CodingTest.src.programmers_kit.repeat;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Greedy3 {
-    public int solution(int[] people, int limit) {
-        int answer = 0;
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for(int person : people){
-            queue.add(person);
-        }
-        while(!queue.isEmpty()){
-            int peopleSum = queue.poll(); //50
-            answer++;
-            while(peopleSum<=limit && queue.size()>0){
-                System.out.println("peopleSum: "+peopleSum);
-                if(peopleSum+queue.peek()>limit){
-                    break;
-                }else{
-                    peopleSum+=queue.poll(); //100
-                }
-            }
+	public int solution(int[] people, int limit) {
+		int answer = 0;
 
-        }
-        return answer;
-    }
+		Arrays.sort(people);
+		int max = people.length - 1;
+		int min = 0;
+		for (int i = max; i >= min; i--) {
+			int peopleCount = people[i]+people[min];
+			answer++;
+			if(peopleCount<=limit) {
+				min++;
+				// 여기서 더 태울 수 있을 때 어떻게 되는 거지?  한 번에 최대 2명씩
+			}
+		}
+
+		return answer;
+	}
 }
