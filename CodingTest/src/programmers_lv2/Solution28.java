8b5d20d5f19920b87211b56524563ef12d5885a8
Jeeho Kim <rlawlgh3245@gmail.com>
2023-08-07 23:18:18 +0900

    23/08/07

--- CodingTest/src/programmers_lv2/Solution28.java
+++ CodingTest/src/programmers_lv2/Solution28.java
@@ -1,8 +1,14 @@
-package CodingTest.src.programmers_lv2;
+package programmers_lv2;
 
 import java.util.*;
 
 public class Solution28 {
+	public static void main(String[] args) {
+		Solution28 s = new Solution28();
+		int[] arr = {1, 3, 2, 5, 4, 5, 2, 3};
+		s.solution(6, arr);
+		
+	}
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         Arrays.sort(tangerine);
@@ -19,16 +25,11 @@ public class Solution28 {
         List<Integer> list = new ArrayList<>(map.keySet());
         list.sort((o1, o2) -> map.get(o2)-map.get(o1));
 
-        for(int num : map.values()){
-            if(k>num){
-                System.out.println("num : "+num);
-                System.out.println("k : "+k);
-
-                k-=num;
+        for(int num : list){
+            if(k>map.get(num)){
+                k-=map.get(num);
                 answer++;
             }else{
-                System.out.println("num : "+num);
-                System.out.println("k : "+k);
                 answer++;
                 return answer;
             }
