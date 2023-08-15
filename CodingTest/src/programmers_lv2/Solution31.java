6381f4b07e706a90ca500962cd0cddf4968fbd10
Jeeho Kim <rlawlgh3245@gmail.com>
2023-08-15 23:18:34 +0900

    23/08/15

--- CodingTest/src/programmers_lv2/Solution31.java
+++ CodingTest/src/programmers_lv2/Solution31.java
@@ -1,60 +1,75 @@
 package programmers_lv2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Solution31 {
-	int regions;
-	int goal;
-	boolean[][] visited;
+
+	List<List<Integer>> list; // roads의 정보를 담고 있음
+	int[] arr; // 임의로 만든 배열로써, 가변
+	final int max = Integer.MAX_VALUE;
 
 	public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-		regions = n;
-		goal = destination;
-		visited = new boolean[n + 1][n + 1];
-		int[] answer = new int[sources.length];
-		List<Integer> list = new ArrayList<>();
 
-		for (int i = 0; i < roads.length; i++) {
-			int[] arr = roads[i];
-			visited[arr[0]][arr[1]] = true;
-			// road에 있는 경우 체크
-			visited[arr[1]][arr[0]] = true;
-			// 반대의 경우도 체크
+		list = new ArrayList<>(); // 초기화
+
+		for (int i = 0; i < n + 1; i++) {
+			list.add(new ArrayList<>()); // 지역의 길이만큼 list가 할당 됨
 		}
 
-		for (int position : sources) {
-			if (position == destination) {
-				// 현재 위치와 destination과 같은 경우
-				list.add(0);
-				continue;
-			}
-			int path = BFS(position, 0, n);
-			list.add(path);
+		for (int[] road : roads) {
+			int startNum = road[0];
+			int endNum = road[1];
 
+			list.get(startNum).add(endNum);
+			list.get(endNum).add(startNum);
+			// 연결된 점들이 표시, 복수 값 저장 가능
+
+		}
+
+		arr = new int[n + 1];
+		// 지역의 수를 가지고 있는 배열
+		// 목적지와 연결된 노드들의 가중치가 더해짐
+		Arrays.fill(arr, max);
+		// arr의 모든 수를 우선 가장 큰 수로 집어넣음
+		dijkstra(destination);
+		int[] answer = new int[sources.length];
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = (arr[sources[i]] < max) ? arr[sources[i]] : -1;
+			// source에 있는 값들이 arr에 있는 경우, default 값인 max 보다 작다면 연결되었다는 뜻
+			// source에 해당하는 arr 값을 반환
+			// max 인경우엔 연결 안 되었으므로 -1 반환 
 		}
 
 		return answer;
+
 	}
 
-	private int BFS(int startNum, int level, int result) {
-		if(startNum==goal) {
-			result = Math.min(result, level);
-			
-		}
-		
-		for (int i = 1; i <= regions; i++) {
-			if(startNum==i) {
-				continue;
+	private void dijkstra(int destination) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(destination);
+		arr[destination] = 0;
+
+		while (!queue.isEmpty()) {
+			int des = queue.poll();
+
+			for (int i = 0; i < list.get(des).size(); i++) {
+				// 목적지에 연결된 지역들만큼 검증
+				int cnntedAtDes = list.get(des).get(i);
+				// 목적지에 연결된 지역들을 하나씩 뽑음
+				if (arr[cnntedAtDes] > arr[des] + 1) {
+					// 연결이 되었다면
+					arr[cnntedAtDes] = arr[des] + 1;
+					// 하나 연결 (가중치)
+					queue.add(cnntedAtDes);
+					// 다음 노드와 연결된 것을 찾음
+				}
 			}
-			if(visited[startNum][i]) {
-				visited[i][startNum]=false;
-				// 연결되있기 때문에 한쪽 부분 체크 해제
-				BFS(i, level++, result);
-				visited[i][startNum]=true;
-			}
 		}
-		
-		return 0;
+
 	}
+
 }
