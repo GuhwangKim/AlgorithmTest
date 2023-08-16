66ed73d9037a421129f23183be7202b88f360cec
Jeeho Kim <rlawlgh3245@gmail.com>
2023-08-16 21:58:33 +0900

    23/08/16

--- CodingTest/src/programmers_lv2/Solution32.java
+++ CodingTest/src/programmers_lv2/Solution32.java
@@ -1,28 +1,45 @@
-package CodingTest.src.programmers_lv2;
+package programmers_lv2;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Solution32 {
-    int answer = -1;
+	
     public int solution(int[] topping) {
-
-        /**
-         * 2조각으로 나눔
-         * 순서는 바꿀 수 없음
-         * 종류의 수가 같으면 동일
-         * 안되면 0 반환
-         * */
-
-        int halfPostion = topping.length/2;
-
-        HashSet
-
-        for (int i = 0; i < halfPostion; i++) {
-
-        }
-
-
+    	int answer = 0 ;
+    	Map<Integer, Integer> A = new HashMap<Integer, Integer>();
+    	Set<Integer> B = new HashSet<Integer>();
+    	
+    	// 각 토핑의 개수를 구해줌 
+    	for(int top : topping) {
+    		if(A.containsKey(top)) {
+    			A.put(top, A.get(top)+1);
+    			// 같은게 있다면
+    		} else {
+    			A.put(top, 1);
+    			// 없다면 1 더해줌, 
+    		}
+    	}
+    	
+    	for(int top : topping) {
+    		A.put(top, A.get(top)-1);
+    		// 하나를 빼서 
+    		B.add(top);
+    		// B에 줌 
+    		if(A.get(top)==0) {
+    			// B에 다 주었다면 
+    			A.remove(top); 
+    			// 삭제
+    		}
+    		if(A.size()==B.size()) {
+    			answer++;
+    		}
+    	}
+    	
         return answer;
     }
+	
 
 }
