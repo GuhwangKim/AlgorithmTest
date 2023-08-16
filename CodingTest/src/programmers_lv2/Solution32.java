61ec581cc54e20d3399e09983027e94398b9a19b
JeehoKim <rlawlgh3245@gmail.com>
2023-08-16 13:07:30 +0900

    23/08/16

---
+++ CodingTest/src/programmers_lv2/Solution32.java
@@ -0,0 +1,28 @@
+package CodingTest.src.programmers_lv2;
+
+import java.util.HashSet;
+
+public class Solution32 {
+    int answer = -1;
+    public int solution(int[] topping) {
+
+        /**
+         * 2조각으로 나눔
+         * 순서는 바꿀 수 없음
+         * 종류의 수가 같으면 동일
+         * 안되면 0 반환
+         * */
+
+        int halfPostion = topping.length/2;
+
+        HashSet
+
+        for (int i = 0; i < halfPostion; i++) {
+
+        }
+
+
+        return answer;
+    }
+
+}
