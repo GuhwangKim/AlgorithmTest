3f68343fcd06726ab53f59c153bcec3591a66916
JeehoKim <rlawlgh3245@gmail.com>
2023-08-22 13:09:30 +0900

    23/08/22

---
+++ CodingTest/src/programmers_kit/repeat/StackQueue5.java
@@ -0,0 +1,24 @@
+package CodingTest.src.programmers_kit.repeat;
+
+public class StackQueue5 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        /**
+         * answer에 나올 수 있는 수는 prices idx
+         * */
+
+        for (int i = 0; i < prices.length; i++) {
+            int result = 0;
+            for (int j = i+1; j < prices.length; j++) {
+                result++;
+                if(prices[i]>prices[j]){
+                    answer[i]= result;
+                    break;
+                }
+            }
+        }
+
+        return prices;
+    }
+}
