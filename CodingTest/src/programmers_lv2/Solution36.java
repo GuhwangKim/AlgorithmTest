9fb1a6b043cc462a6c5d248720b4632cdcf39a25
Jeeho Kim <rlawlgh3245@gmail.com>
2023-08-24 23:49:30 +0900

    23/08/24

--- CodingTest/src/programmers_lv2/Solution36.java
+++ CodingTest/src/programmers_lv2/Solution36.java
@@ -20,20 +20,19 @@ public class Solution36 {
 	public int solution(String[] want, int[] number, String[] discount) {
 		answer = 0;
 
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
-		for (int i = 0; i < want.length; i++) {
-			map.put(want[i], number[i]);
-		}
-
 		for (int i = 0; i <= discount.length - 10; i++) {
-			calculate(discount, i, map);
+			calculate(discount, i, want, number);
 		}
 		return answer;
 	}
 
-	private void calculate(String[] discount, int idx, HashMap<String, Integer> map) {
-
+	private void calculate(String[] discount, int idx, String[] want, int[] number) {
 		int cnt = 1; // 첫째날 10일동안 연속으로 계속될 예정
+
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 0; i < want.length; i++) {
+			map.put(want[i], number[i]);
+		}
 
 		while (cnt <= 10) {
 			if (null == map.get(discount[idx])) {
@@ -41,26 +40,26 @@ public class Solution36 {
 				cnt++;
 				continue;
 			}
+			if (map.containsKey(discount[idx])) {
+				// map에 key에 존재하는 게 있다면
+				int value = map.get(discount[idx]);
+				 map.put(discount[idx], value-1);
+			}
 			if (map.get(discount[idx]) == 0) {
 				map.remove(discount[idx]);
 				// 값을 다 썼다면 없애버림
 			}
-			if (map.containsKey(discount[idx])) {
-				// map에 key에 존재하는 게 있다면
-				int value = map.get(discount[idx]);
-
-				map.put(discount[idx], value - 1);
-				// 하나를 빼서 put 에 넣어줌
-			}
 			idx++;
 			cnt++;
 		}
-		if (map.size() == 0) {
+		
+		
+		if (map.size()==0) {
 			answer++;
 			// map이 없다면 다 산것이므로 더함
 			return;
 		}
-
+		
 	}
 
 }
