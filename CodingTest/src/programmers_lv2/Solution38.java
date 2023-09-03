7179f8478873c9f9edceedf886b9edb096992a5b
Jeeho Kim <rlawlgh3245@gmail.com>
2023-09-03 09:37:02 +0900

    23/09/03

--- CodingTest/src/programmers_lv2/Solution38.java
+++ CodingTest/src/programmers_lv2/Solution38.java
@@ -1,53 +1,63 @@
 package programmers_lv2;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution38 {
-	boolean[] visited;
+	static ArrayList<Integer>[] childs;
+	static int[] Info;
+	static int maxSheepCnt = 0;
+	
 	public int solution(int[] info, int[][] edges) {
-		visited = new boolean[info.length];
-		
-		Arrays.sort(edges, (o1, o2) -> o1[0]-o2[0]); // 0번째 인덱스를 기준으로 오름차순 
-		visited[0] = true;
-		// 가장 첫번째 부모는 방문함 = 무조건 양 cnt+1;
-		
-		int answer = BFS(edges[0][1], edges, info, 1, 0);
-		
-		
-		for (int i = 0; i < edges.length; i++) {
-			int parent = edges[i][0];
-			int child = edges[i][1];
-			
-			int sCnt = 1;
-			int wCnt = 0;
-			
-			if(wCnt >= sCnt) { // 조건 불일치 
-				break;
+		Info = info;
+		childs = new ArrayList[info.length];
+		for(int[] arr : edges) {
+			int parent = arr[0];
+			int child = arr[1];
+			if(childs[parent]==null) {
+				// parent 인덱스에 속한 child Integer 값들을 담아줌 => 배열의 형태로 
+				childs[parent] = new ArrayList<Integer>();
 			}
-			
-			if(visited[i]) {
-				// 방문하지 않았다면 더해줌 
-			}
+			childs[parent].add(child);
 			
 		}
 		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(0);
+		DFS(0, 0, 0, list);
 		
-		return answer;
+		return maxSheepCnt;
 	}
-	private int BFS(int nextParent, int[][] edges, int[] info, int sheep, int wolf) {
-		
-		
-		if(info[nextParent]>0) {
-			// 늑대인 경우 
-			wolf++;
+
+	private static void DFS(int idx, int sheepCnt, int wolfCnt, List<Integer> childList) {
+		if(Info[idx]==0) {
+			sheepCnt++;
+		// 양인 경우 양 개수 증가 
+		}else {
+			wolfCnt++;
 		}
 		
-		if()
+		if(wolfCnt>=sheepCnt) {
+			return;
+			// 늑대의 개수가 양의 개수보다 많거나 같으면 끝 
+		}
+		maxSheepCnt = Math.max(sheepCnt, maxSheepCnt);
 		
+		// 다음 탐색 갱신
+		List<Integer> list = new ArrayList<Integer>();
+		list.addAll(childList);
+		list.remove(Integer.valueOf(idx));
+		// 현재 idx 는 제거하고 다른 것들을 타야함 
+		if(childs[idx]!=null) {
+			for(int child : childs[idx]) {
+				list.add(child);
+				// 부모 노드에 연결되어있는 child 노드의 인덱스들을 list에 담음 
+			}
+		}
 		
-		return wolf;
-		
-		
+		for(int next : list) {
+			DFS(next, sheepCnt, wolfCnt, list);
+		}
 		
 	}
 }
