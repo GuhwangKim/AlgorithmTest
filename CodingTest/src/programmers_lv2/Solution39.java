76d0f6deb6ec8f8f368d81f2f16e10644304da89
Jeeho Kim <rlawlgh3245@gmail.com>
2023-09-05 23:29:49 +0900

    23/09/05

--- CodingTest/src/programmers_lv2/Solution39.java
+++ CodingTest/src/programmers_lv2/Solution39.java
@@ -1,76 +1,61 @@
 package programmers_lv2;
 
 public class Solution39 {
-	int len = 11, maxDiff = 0;
+	int max;
+	int len = 11;
 	int[] maxRyan = new int[len];
 
 	public int[] solution(int n, int[] info) {
-		int[] ryan = new int[len];
-		getArcheryScore(n, info, ryan, 0); // 인덱스이자 점수
-
-		return maxDiff > 0 ? maxRyan : new int[] { -1 };
-		// 0보다 크면 라리언 배열 그렇지 않으면 -1
+		DFS(0, n, 0, new int[n], info);
+		// max == 0 이라면 라이언이 이긴적 없음 
+		
+		
+		return max == 0? new int[] {-1} : maxRyan;
+		
 	}
 
-	private void getArcheryScore(int n, int[] info, int[] ryan, int score) {
-		if (n == 0 || score == len) {
-			// 화살을 소진했거나 스코아 바깥으로 나가는 경우
-			setMaxRyanDiff(info, ryan);
-			return;
-		}
-
-		int arrow = 0;
-		if (info[score] < n) {
-			// 화살의 개수 보다 작을때
-			arrow = info[score] + 1;
-			// 화살의
-		} else if (score == len - 1) {
-			arrow = n;
-			// 10점일 때 화살은 가장 많이
-		}
-
-		int tmp = info[score]; // 아파치의 점수
-		if (arrow > 0) {
-			// 화살 남음
-			info[score] = 0; // 아피치 점수 0
-			ryan[score] = arrow;
-			getArcheryScore(n, info, ryan, score + 1); // 다음점수 계산
-		}
-	}
-
-	private void setMaxRyanDiff(int[] info, int[] ryan) {
-		int sum = 0;
-		// 어피치 라이언의 점수 차 계산
-		for (int i = 0; i < len; i++) {
-			if (info[i] > 0) {
-				sum -= 10 - i;
+	private void DFS(int start, int rest, int idx, int[] result, int[] info) { // result는 화살의 개수 만큼 
+		if(idx == rest) {
+			// 화살을 남은 개수 만큼 쏜경우 
+			int[] info2 = new int[len];
+			for(int score : result) {
+				// 라이언의 화살 맞춘 개수를 배열에 집어넣음 ex 7점  -> 3번 인덱스 
+				info2[10-score]++;
 			}
-		}
-		for (int i = 0; i < len; i++) {
-			if (info[i] > 0) {
-				sum += 10 - i;
-			}
-		}
-
-		if (this.maxDiff < sum) {
-			for (int i = 0; i < len; i++) {
-				this.maxRyan[i] = ryan[i];
-				this.maxDiff = sum;
-			}
-		} else if (this.maxDiff == sum) {
-			for (int i = len - 1; i >= 0; i++) {
-				if (this.maxRyan[i] < ryan[i]) {
-					for (int j = 0; j < len; j++) {
-						this.maxRyan[j] = ryan[j];
-						break;
-					}
-				} else if (this.maxRyan[i] > ryan[i]) {
-					break;
+			
+			int ryan = 0;
+			int apeach = 0;
+			
+			for (int i = 0; i < 11; i++) {
+				//info[0] = 10 / info[1] = 9
+				int score = 10-i;
+				if(info2[i] > info[i]) {
+					// 맞춘 과녁의 개수가 라이언이 더 클 때 
+					ryan += score;
+					// 라이언이 점수 얻음 
+				} else if (info2[i] < info[i]) {
+					// 과녁의 개수가 어피치가 더 클 때 
+					apeach += score;
+					// 어피치가 점수 얻음 
+				} else if (info[i]!=0 && info2[i]==info[i]) {
+					// 어피치 과녁의 개수가 있는데, 과녁의 개수가 같을 때
+					apeach += score;
+					// 어피치가 점수 얻음 
+					
 				}
 			}
-
+			if(ryan-apeach>max) {
+				max = ryan-apeach;
+				// 라이언과 어피치의 과녁의 합의 차가 크다 = 라이언이 더 높은 득점으로 이기다 
+				maxRyan = info2;
+			}
+			return;
 		}
-
+		for (int i = start; i < 11; i++) {
+			// 완전 탐색 start 0 일때 0 인덱스 즉 10점에 0 들어감 -> idx가 n 화살보다 작은 경우에 
+			result[idx] = i;
+			DFS(i, rest, idx+1, result, info);
+		}
 	}
 
 }
