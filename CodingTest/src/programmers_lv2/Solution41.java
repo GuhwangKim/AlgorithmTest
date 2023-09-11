0f119ad3deffef04c46a0feb28b01614c33ff821
Jeeho Kim <rlawlgh3245@gmail.com>
2023-09-11 23:14:19 +0900

    23/09/11

--- CodingTest/src/programmers_lv2/Solution41.java
+++ CodingTest/src/programmers_lv2/Solution41.java
@@ -1,96 +1,53 @@
 package programmers_lv2;
 
-import java.util.LinkedList;
 import java.util.Stack;
 
 public class Solution41 {
-	static class Node {
-		Node prev = null;
-		Node next = null;
-		boolean isDelete;
-	}
 
 	public String solution(int n, int k, String[] cmd) {
-		Node[] nodeArr = new Node[10000];
+		Stack<Integer> removeHistory = new Stack<Integer>();
 
-		nodeArr[0] = new Node();
-
-		for (int i = 1; i < n; i++) {
-			// 현재 Node의 앞, 뒤 정보 담아주기
-			nodeArr[i] = new Node();
-			nodeArr[i].prev = nodeArr[i - 1];
-			nodeArr[i - 1].next = nodeArr[i];
-		}
-
-		Stack<Node> delete = new Stack<>();
-		// 지울 history 쌓기
-		Node now = nodeArr[k];
-		// 현재 위치
-
+		int table_size = n;
 		for (String each : cmd) {
-			if (each.charAt(0) == 'D') {
-				// 위
-				int cnt = Integer.parseInt(each.substring(2));
-				for (int i = 0; i < cnt; i++) {
-					now = now.next;
-					// 현재 위치가 -1 씩 2번이 되고 있음
+			char c = each.charAt(0);
+			if (c == 'D') {
+				// 아래로
+				k += Integer.parseInt(each.substring(2));
+
+			} else if (c == 'U') {
+				k -= Integer.parseInt(each.substring(2));
+
+			} else if (c == 'C') {
+				removeHistory.add(k);
+				// 현재의 인덱스 집어넣음
+				table_size--;
+				// 하나 제거함
+				if (k == table_size) {
+					// 가장 끝에 있을 때
+					k--;
+					// 현재위치에서 한단계 올라감
 				}
 
-			} else if (each.charAt(0) == 'U') {
-				// 아래
-				int cnt = Integer.parseInt(each.substring(2));
-				for (int i = 0; i < cnt; i++) {
-					now = now.prev;
-					// 현재 위치가 1 씩 2번이 되고 있음
+			} else if (c == 'Z') {
+				if (removeHistory.pop() <= k) {
+					// 복구하는 인덱스가 현재 인덱스보다 작을 때는 변경됨
+					k++;
 				}
-
-			} else if (each.charAt(0) == 'C') {
-				// 삭제 후 아래
-				now.isDelete = true;
-				delete.push(now);
-				// history 쌓아올라옴
-				Node prev = now.prev;
-				Node next = now.next;
-
-				if (prev != null) {
-					// 전 값이 있다면
-					prev.next = next;
-				}
-				if (next != null) {
-					// 다음 값이 있다면
-					next.prev = prev;
-					now = next;
-				} else {
-					// 현재 값은 전 값과 같음
-					now = prev;
-				}
-
-			} else {
-				// 복구
-				Node node = delete.pop();
-				Node prev = node.prev;
-				Node next = node.next;
-
-				node.isDelete = false;
-				if (prev != null) {
-					prev.next = node;
-				}
-				if (next != null) {
-					next.prev = node;
-				}
+				table_size++;
+				// 공통 사이즈가 커짐
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			if (nodeArr[i].isDelete) {
-				sb.append('X');
-			} else {
-				sb.append('0');
-			}
+		for (int i = 0; i < table_size; i++) {
+			sb.append("O");
+		}
+		while (!removeHistory.isEmpty()) {
+			sb.insert(removeHistory.pop().intValue(), "X");
 		}
 
 		return sb.toString();
+
 	}
 
 }
