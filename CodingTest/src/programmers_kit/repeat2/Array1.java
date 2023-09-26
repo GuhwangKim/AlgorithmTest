9c177d8d49ff0a0119da8a41ac41ca42b97dfe9a
Jeeho Kim <rlawlgh3245@gmail.com>
2023-09-26 22:54:21 +0900

    23/09/26

---
+++ CodingTest/src/programmers_kit/repeat2/Array1.java
@@ -0,0 +1,16 @@
+package programmers_kit.repeat2;
+
+import java.util.Arrays;
+
+public class Array1 {
+	public String solution(int[] numbers) {
+		String[] stringNum = new String[numbers.length];
+		
+		
+		
+		return null;
+		
+	}
+	
+
+}
