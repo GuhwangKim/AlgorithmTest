3ec337cd05abf527ca356b6c64f537097e2cd06e
JeehoKim <rlawlgh3245@gmail.com>
2023-09-27 13:22:17 +0900

    23/09/27

---
+++ CodingTest/src/programmers_lv2/Solution45.java
@@ -0,0 +1,13 @@
+package CodingTest.src.programmers_lv2;
+
+public class Solution45 {
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = {};
+        /**
+         * 1. 빈 값을 가진 테이블을 만듦
+         * 2. 숫자가 바뀔 때
+         * */
+        return answer;
+    }
+
+}
