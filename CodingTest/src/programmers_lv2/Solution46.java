9cf4c75ceabd8ff6b58914414fce0284b45cb519
Jeeho Kim <rlawlgh3245@gmail.com>
2023-10-10 16:41:00 +0900

    23/10/10

--- CodingTest/src/programmers_lv2/Solution46.java
+++ CodingTest/src/programmers_lv2/Solution46.java
@@ -1,4 +1,4 @@
-package CodingTest.src.programmers_lv2;
+package programmers_lv2;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -7,45 +7,43 @@ import java.util.Stack;
 public class Solution46 {
     public int solution(String s) {
         int answer = 0;
-        Queue<Character> queue = new LinkedList<>();
-
-        // queue 에 글자에 있는 괄호들을 각각 담음
         for (int i = 0; i < s.length(); i++) {
-            queue.add(s.charAt(i));
+        	// 1. 총 5번을 진행함, 첫번째수를 가장 끝으로 보내는 일 
+        	Stack<Character> stack = new Stack<Character>();
+            
+            for (int j = 0; j < s.length(); j++) {
+            	// 3. 처음부터 돌면서 조건에 만족하는지 확인함 
+            	char compare = s.charAt(j);
+            	if(stack.isEmpty()) {
+            		if(compare==')' || compare=='}' || compare== ']') {
+            			break; // 조건에 만족하지 않는 경우, 조건문 탈출 
+            		} else {
+                        // 2. 가장 첫번째 수를 집어넣음 
+                        stack.add(compare);
+            		}
+            	}
+            	
+				if(stack.peek()=='(' && compare==')') {
+					stack.pop();
+					continue;
+				} else if (stack.peek()=='{' && compare=='}') {
+					stack.pop();
+					continue;
+				} else if (stack.peek()=='[' && compare==']') {
+					stack.pop();
+					continue;
+				}
+			}
+			
+			if(stack.size()==0) {
+				answer++;
+			}
+			// 4. 0번쨰 있는 것을 가장 마지막으로 이동시킴
+			s = s.substring(1, s.length()) + s.charAt(0);
+			
         }
-
-        for (int i = 0; i < s.length(); i++) {
-            char compare = queue.poll();
-            checking(compare, queue);
-        }
-
-
         return answer;
     }
 
-    private void checking(char compare, Queue<Character> queue) {
-        // 가장 처음 뽑은 값이 ]}) 인경우 무조건 안됨
-        if(compare == ']' || compare == '}' || compare == ')'){
-            queue.add(compare);
-            // 뽑은 숫자를 가장 마지막에 집어넣고 넘김
-            return;
-        }
 
-        // 새롭게 검증할 새로운 queue를 만듦
-        Queue<Character> queue2 = new LinkedList<>();
-        for(char ch : queue){
-            queue2.add(ch);
-        }
-        Stack<Character> stack = new Stack<>();
-        stack.add()
-
-        while(!queue2.isEmpty()){
-
-
-        }
-
-
-
-
-    }
 }
