e6fae4108e8d4ca48f27ff063bc9725be8b6bcfb
JeehoKim <rlawlgh3245@gmail.com>
2023-10-19 13:13:16 +0900

    23/10/19

---
+++ CodingTest/src/programmers_lv2/Solution50.java
@@ -0,0 +1,10 @@
+package CodingTest.src.programmers_lv2;
+
+public class Solution50 {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+
+
+        return answer;
+    }
+}
