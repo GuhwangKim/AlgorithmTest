2a11228b71086871c213a3b8aa2cc2a74c479beb
JeehoKim <rlawlgh3245@gmail.com>
2023-11-01 12:58:00 +0900

    23/11/01

--- CodingTest/src/programmers_lv2/Solution55.java
+++ CodingTest/src/programmers_lv2/Solution55.java
@@ -1,60 +1,82 @@
 package CodingTest.src.programmers_lv2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution55 {
-    int[] dx = {-1,0,1,0};
-    int[] dy = {0,-1,0,1};
-    boolean[][] visited;
-    int direct = 1;
-    int corner = 0;
+    int[][] d = {{-1, 0}, {1,0}, {0,1}, {0, -1}};
+    boolean[][][] visited;
     int num;
-    int answer = 0;
     public int solution(int[][] board) {
         num = board.length;
-        visited = new boolean[num][num];
-        
-    	/*
-        * 0,0 에서 상하좌우 해봄
-        * 쭉 가다가 막힌 곳 있는데, 그 뒤로 상하좌우 가 간다면 코너
-        * n , n 까지 갔을 때 직진 코너 값
-        * */
-        int x = 0, y = 0;
+        visited = new boolean[num][num][4];
 
-        DFS(0, 0, 0, board);
-        
-        
-
-        return answer;
+        return BFS(board);
     }
-	private void DFS(int curXidx, int curYidx, int blockCnt, int[][] board) {
-		if(curXidx == num-1 && curYidx == num-1) {
-        	// 끝 
-			answer = Math.min(answer, (direct*100)+(corner*500));
-			return;
-        }
-		
-		
-		for (int i = 0; i < 4; i++) {
-            int nx = curXidx + dx[i];
-            int ny = curYidx + dy[i];
-            
-            // 상하좌우 이동
-            if(nx<0 || ny<0 || nx>=num || ny>= num || board[nx][ny]==1 || visited[nx][ny]){
-                // 좌표가 가지고 있는 인덱스 밖을 넘어서거나 벽이 있는 경우거나 이미 방문한 경우 
-            	// 이런 경우가 3개가 함께 안된다면 그땐 코너가 됨
-            	// 무조건 1곳은 빠져나갈 수 있게 구성이 되어있음 
-            	blockCnt++;
-            	continue;
-            }
-            if(blockCnt==3) {
-            	// 3면이 다 막힌 상황 
-            	corner++;
-            }
-            visited[nx][ny] = true;
-            DFS(nx,ny,0,board);
-            // 카운트 원상태 
-            visited[nx][ny] = false;
 
+    private int BFS(int[][] board) {
+        int x = 0, y=0, direction = -1, cost=0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(x, y, direction, cost));
+
+        int minCost= Integer.MAX_VALUE;
+
+        while(!queue.isEmpty()){
+            Node now = queue.poll();
+            if(now.row == num-1 && now.column == num-1){
+                // 현재 인덱스가 가장 마지막에 와있다면
+                minCost = Math.min(minCost, now.cost);
+            }
+            for (int i = 0; i < 4; i++) {
+                // 4 방향으로 이동시킴
+                int nextRow = now.row + d[i][0];
+                int nextColumn = now.column +d[i][1];
+
+                if(nextRow <0 || nextColumn <0 || nextRow>=num || nextColumn>=num || board[nextRow][nextColumn]==1){
+                    // 이동한 값에 갈 수 없음
+                    // 방향을 틀거나, 혹은 직진
+                    continue;
+                }
+                // *** 이 부분이 메인
+                // 만약 앞에 continue를 하지 않았다면 계속 직진해서 방향이 같을 것
+                // 그러나 continue를 했다면 방향이 달라짐
+                int nextCost = now.cost;
+                if(now.direction == -1 || now.direction == i){
+                    // direction이 처음 나오는 경우나 direction이 바뀌지 않음
+                    nextCost += 100; // 직진
+                }else{
+                    nextCost += 600; // 코너
+                }
+                // 앞으로 더 어떻게 가야할까?
+                // 매 행렬마다 거기까지 도달할 때의 값을 넣어주고, 최소를 비교
+                if(!visited[nextRow][nextColumn][i] || board[nextRow][nextColumn] >= nextCost){
+                    // 방문을 하지 않았거나
+                    // 방문을 했더라도 현재까지 축적된 cost의 값이 더 작거나
+                    // 더 크더라도 방문을 하지 않았거나
+                    queue.add(new Node(nextRow, nextColumn, i, nextCost ));
+                    visited[nextRow][nextColumn][i] = true;
+                    // 방문
+                    board[nextRow][nextColumn] = nextCost;
+                    // 해당 matrix 까지 가는 비용을 저장함
+                }
+            }
         }
-		
-	}
+        return minCost;
+
+    }
+
+    private class Node {
+        int row, column, direction, cost;
+
+        public Node(int row, int column, int direction, int cost){
+            this.row = row;
+            this.column = column;
+            this.direction = direction;
+            this.cost = cost;
+        }
+
+    }
+
 }
+
+
