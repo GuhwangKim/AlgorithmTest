bd31e2ee74430ff9f093e1c806738573c65fbcde
Jeeho Kim <rlawlgh3245@gmail.com>
2023-11-04 00:07:49 +0900

    23/11/03

--- CodingTest/src/programmers_lv2/Solution56.java
+++ CodingTest/src/programmers_lv2/Solution56.java
@@ -4,25 +4,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution56 {
+	public static void main(String[] args) {
+		Solution56 s = new Solution56();
+		String[] arr = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+		s.solution(arr);
+		
+	}
+	
 	public int[] solution(String[] gems) {
         int[] answer = new int[2];
-        /**
-         * 순서대로 구간 안에 안에 있는 모든 종류를 포함해야함 
-         * 그때 시작인덱스 ~ 끝 인덱스를 담아줌 
-         * 시작 인덱스는 가장 작은 수로 
-         * 
-         * */
-        
-        // 1. set으로 해서 gems 안에 있는 종류의 총 개수를 구하고 
-        // 2. 1부터 시작해서, 총 개수를 구할 때까지의 startIdx, endIdx 를 지정함 
-        // 3. 두 인덱스의 차가 작은 것 -> startIdx가 작은 것이 답임
+        answer[0] = 0;
+        answer[1] = gems.length;
         
         Set<String> set = new HashSet<String>();
         for(String gem : gems) {
         	set.add(gem);
+        	
         }
-        
-        
         int typesTotalCnt = set.size();
         if(typesTotalCnt==1) {
         	// 1개만 존재함 
@@ -30,10 +28,7 @@ public class Solution56 {
         	answer[1]=1;
         	return answer;
         }
-        
         int lastIdx = gems.length - typesTotalCnt;
-        
-        
         for (int i = 0; i <= lastIdx ; i++) {
         	Set<String> set2 = new HashSet<String>();
         	set2.add(gems[i]);
@@ -47,8 +42,11 @@ public class Solution56 {
 				set2.add(gems[j]);
 				if(set2.size()==typesTotalCnt) {
 					// 총 개수와 같다면 
-					answer[0]= Math.min(answer[0], i);
-					answer[1]= Math.min(answer[1], j);
+					if((answer[1]-answer[0]>j-i)) {
+						// 차가 더 작다면 
+						answer[0] = i+1;
+						answer[1] = j+1;
+					}
 				}
 			}
 		}
