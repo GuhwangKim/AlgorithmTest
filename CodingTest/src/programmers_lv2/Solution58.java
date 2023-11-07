e8297b9a1b7c9da1210d50b1e5596c92e2c1c77c
JeehoKim <rlawlgh3245@gmail.com>
2023-11-07 13:04:11 +0900

    23/11/07

---
+++ CodingTest/src/programmers_lv2/Solution58.java
@@ -0,0 +1,20 @@
+package CodingTest.src.programmers_lv2;
+
+public class Solution58 {
+    public int solution(String[] user_id, String[] banned_id) {
+        int answer = 0;
+        /*
+        * 1. b 배열에 원소들의 길이 기준으로 나열함
+        * 2. u 배열에서 원소 값을 하나씩 빼서 b 배열과 비교함
+        * 3. b>u 원소간의 길이가 큰 경우 break 그 다음 u 원소로 넘어감
+        * 4. 같은 경우 글자 하나씩 비교함
+        *   비교했는데 값이 같다면
+        * 5. 그렇지 않은 경우 0 리턴
+        *
+        * */
+
+
+
+        return answer;
+    }
+}
