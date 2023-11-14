494ca1c86c6429f37e8c1cb9273fe365736197bd
JeehoKim <rlawlgh3245@gmail.com>
2023-11-14 13:12:40 +0900

    23/11/14

--- CodingTest/src/programmers_lv2/Solution59.java
+++ CodingTest/src/programmers_lv2/Solution59.java
@@ -1,69 +1,68 @@
 package CodingTest.src.programmers_lv2;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Solution59 {
     public long solution(String expression) {
-        long answer = 0;
-        /*
-        * 총 6가지의 우선순의 경우의 수가 있음
-        * expression이 그렇게 길지 않음 처음부터 하나씩 추출하자
-        * + - * 를 중심으로 나누고,
-        * 연산자는 홀수 인덱스에만 존재한다
-        * loop를 홀수 연산자으로 반복한다
-        * loop를 돌 떄마다 총합의 연산자의 최소 값을 비교해서 교체한다
-        * 
-        *
-        * arr에 넣어주고, visited 까지 같이 잡아줌
-        * */
-        String[] count = expression.split("\\+|-|\\*");
-        String[] arr = new String[count.length + ]
+        long answer = Long.MIN_VALUE;
 
-        StringBuilder stringBuilder = new StringBuilder();
+        String op[][] = { { "+", "-", "*" }, { "+", "*", "-" }, { "-", "*", "+" },
+                { "-", "+", "*" }, { "*", "-", "+" }, { "*", "+", "-" } };
+
+        ArrayList<String> list = new ArrayList<>();
+        
+        int start = 0;
         for (int i = 0; i < expression.length(); i++) {
-            if(expression.charAt(i)=='+' || expression.charAt(i)=='-' || expression.charAt(i)=='*'){
-                // 기호가 있다면 건너뛰어야 하고, 이 기준으로 나누고, 초기화 시킴
-                list.add(String.valueOf(stringBuilder));
-                list.add(String.valueOf(expression.charAt(i)));
-                stringBuilder = new StringBuilder();
-                continue;
+            if(expression.charAt(i) == '-' || expression.charAt(i) == '+' || expression.charAt(i) == '*'){
+                // 연산자라면
+                list.add(expression.substring(start, i));
+                // 연산자 앞에 숫자를 집어넣음
+                list.add(expression.charAt(i)+"");
+                // 연산자를 집어넣음
+                start = i+1;
+                // 시작 포인트를 연산자 그 다음으로 함
             }
-            stringBuilder.append(expression.charAt(i));
         }
-        String[] operation = {"+", "-", "*"};
-        for (int i = 0; i < operation.length; i++) {
-            // + - * 순으로 넘겨봄
-            DFS(operation[i], list);
+        list.add(expression.substring(start));
+
+        for (int i = 0; i < op.length; i++) {
+            // 연산자의 조합 개수
+            ArrayList<String> subList = new ArrayList<>(list);
+            for (int j = 0; j < 3; j++) {
+                // 연산자의 개수 3개
+                for (int k = 0; k < subList.size(); k++) {
+                    if(op[i][j].equals(subList.get(k))){
+                        // 나온 숫자가 연산자일 때
+                        subList.set(k-1, cal(subList.get(k-1),subList.get(k), subList.get(k+1)));
+                        // k -1 숫자 k 연산자 k+1 숫자를 넘겨서 계산한 후 , k-1 위치에 저장
+                        subList.remove(k);
+                        // k번쨰를 삭제
+                        subList.remove(k);
+                        // 2개를 연산했으므로, 숫자 2개를 제외시킴
+                        k--;
+                    }
+                }
+            }
+            // 연산자 조합의 개수 로 나온 값을 비교함
+            // 계산 후엔 값이 하나만 남음
+            answer = Math.max(answer, Math.abs(Long.parseLong(subList.get(0))));
         }
+
 
         return answer;
     }
 
-    private void DFS(String firstOperation, List<String> list) {
-
-        // 가장 먼저 나와야하는 operation
-        for (int i = 1; i < list.size()-1; i+=2) {
-            // 연산에 대해서만 비교함
-            if(list.get(i)==firstOperation){
-                // 처음에 해아하는 연산
-                int frontNum = Integer.parseInt(list.get(i-1));
-                int backNum = Integer.parseInt(list.get(i+1));
-
-                String operatedNum ="";
-                if(firstOperation.equals("+")){
-                    operatedNum = String.valueOf(frontNum+backNum);
-                } else if (firstOperation.equals("-")) {
-                    operatedNum = String.valueOf(frontNum-backNum);
-                } else {
-                    operatedNum = String.valueOf(frontNum*backNum);
-                }
-
-                list.re
-
-
-            }
+    private String cal(String num1, String op, String num2) {
+        long n1 = Long.parseLong(num1);
+        long n2 = Long.parseLong(num2);
+        if(op.equals("+")){
+            return n1+n2+"";
         }
-
+        if(op.equals("-")){
+            return n1-n2+"";
+        }
+        return n1*n2+"";
     }
 }
