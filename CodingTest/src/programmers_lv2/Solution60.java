c5df0192d7332ba670c8249403408a67624ec542
Jeeho Kim <rlawlgh3245@gmail.com>
2023-11-14 23:09:27 +0900

    23/11/14

---
+++ CodingTest/src/programmers_lv2/Solution60.java
@@ -0,0 +1,22 @@
+package programmers_lv2;
+
+public class Solution60 {
+	public int solution(int[] stones, int k) {
+		int answer = 0;
+		/*
+		 * 0이면 건너뜀 +1
+		 * 건너뜀의 개수가 k 미만일 때 (k와 같아지면 끝 못 감) 
+		 * 건넘, 
+		 * 
+		 * 0이 아니면 해당 숫자 -1 해줌 
+		 * 거리 +1 해줌 
+		 * 거리 = stones 의 길이와 같다면 answer + 1
+		 * 
+		 * */
+		
+		
+		
+		return answer;
+	}
+
+}
