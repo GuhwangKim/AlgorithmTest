90b8cf62217edc5c8270be66cb5d9d2ea417bb68
JeehoKim <rlawlgh3245@gmail.com>
2023-11-15 13:08:09 +0900

    23/11/15

--- CodingTest/src/programmers_lv2/Solution60.java
+++ CodingTest/src/programmers_lv2/Solution60.java
@@ -3,20 +3,42 @@ package programmers_lv2;
 public class Solution60 {
 	public int solution(int[] stones, int k) {
 		int answer = 0;
-		/*
-		 * 0이면 건너뜀 +1
-		 * 건너뜀의 개수가 k 미만일 때 (k와 같아지면 끝 못 감) 
-		 * 건넘, 
-		 * 
-		 * 0이 아니면 해당 숫자 -1 해줌 
-		 * 거리 +1 해줌 
-		 * 거리 = stones 의 길이와 같다면 answer + 1
-		 * 
-		 * */
-		
-		
-		
+		int min = 1;
+		int max = 200000000;
+
+		// 건널 수 있는 인원 max 가 아직은 크다
+		while(min<=max){
+			// 건널 인원 반 시도
+			int mid = (min + max) / 2;
+			if(canAcrossRiver(stones, k, mid)){
+				// 건널 수 있다면 더 많이 시도해봄
+				min = mid +1;
+				answer = Math.max(answer, mid);
+
+			}else{
+				// 건널 수 없으면 좁힘
+				max = mid-1;
+			}
+		}
 		return answer;
+	}
+
+	private boolean canAcrossRiver(int[] stones, int k, int friends) {
+		int skip = 0;
+		for (int stone : stones){
+			if(stone - friends < 0){
+				// 3명이 건너야 하는데 돌맹이의 현재 가능한 수가 그거보다 작다면 불가능
+				skip++;
+			}else{
+				// 0 이상이면 건널 수 있음, 건넜다면 skip이 reset 됨
+				skip = 0;
+			}
+			if(skip == k) {
+				// k 만큼 건너뛰었으면 못 건넘
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
