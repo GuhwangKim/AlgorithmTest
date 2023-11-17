d6ee24adc05b381d9bbb7004a726299b1b1b5fec
JeehoKim <rlawlgh3245@gmail.com>
2023-11-17 13:14:51 +0900

    23/11/17

--- CodingTest/src/programmers_lv2/Solution61.java
+++ CodingTest/src/programmers_lv2/Solution61.java
@@ -1,9 +1,13 @@
 package CodingTest.src.programmers_lv2;
 
+import java.math.BigInteger;
+
 public class Solution61 {
     public long solution(int w, int h) {
         long answer = 1;
-
+        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
+        // w 와 h의 최대 공약수
+        answer = ((long)w * (long)h) - ((((long)w/gcd)+((long)h/gcd)-1)*gcd);
 
         return answer;
     }
