eb423135b87005b0b7aef6367f91a23dd64f81f1
Jeeho Kim <rlawlgh3245@gmail.com>
2023-11-21 23:33:08 +0900

    23/11/21

--- CodingTest/src/programmers_lv2/Solution63.java
+++ CodingTest/src/programmers_lv2/Solution63.java
@@ -3,47 +3,80 @@ package CodingTest.src.programmers_lv2;
 import java.util.Stack;
 
 public class Solution63 {
-    public String solution(String p) {
-        String answer = "";
-        /**
-         * 1) right ( left ) 의 개수가 같으면 2 부분으로 나뉨
-         * 2) 두 번쨰 있는 값이 "" 일 때까지 계속해서 반복해야함
-         * 3) "" 값이면 앞에 있는 값을 가장 앞 뒤 를 제거하고 나머지 값들을 반대로 해서 return
-         * */
 
-        int right = 0;
-        String strRight = "";
-        int left = 0;
-        String strLeft = "";
+	public String solution(String p) {
+		String answer = "";
+		if (p.equals("")) {
+			// 입력값이 빈 문자열이라면
+			return "";
+			// 빈문자열 반환함
+		}
 
-        String[] splitTwo = new String[2];
-        for (int i = 0; i < p.length(); i++) {
-           if(p.charAt(i)==')'){
-               left++;
-               strLeft+=p.charAt(i);
-           } else{
-               right++;
-               strRight+=p.charAt(i);
-           }
-           if(left == right){
-               // 개수가 같을 떄 멈춤
-              splitTwo[0] = strLeft; // u
-              splitTwo[1] = strRight; // v
-           }
-        }
+		answer = check(p);
 
-        // u를 검증함 (올바른 문자열인지 아닌지)
-        Stack<Character> stack = new Stack<>();
-        stack.add(strLeft.charAt(0));
-        for (int i = 0; i < strLeft.length(); i++) {
+		return answer;
+	}
 
-        }
-        
+	private String check(String p) {
+		if (p.equals("")) {
+			// 입력값이 빈 문자열이라면
+			return "";
+			// 빈문자열 반환함
+		}
+		int right = 0;
+		String strRight = "";
+		int left = 0;
+		String strLeft = "";
 
+		for (int i = 0; i < p.length(); i++) {
+			if (p.charAt(i) == ')') {
+				left++;
+				strLeft += p.charAt(i);
+			} else {
+				right++;
+				strRight += p.charAt(i);
+			}
+			if (left == right) {
+				// 개수가 같을 떄 멈춤
+				break;
+			}
+		}
 
+		// u를 검증함 (올바른 문자열인지 아닌지)
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < strLeft.length(); i++) {
+			if (stack.isEmpty()) {
+				stack.add(strLeft.charAt(i));
+				continue;
+			}
+			if (stack.peek() == strLeft.charAt(i)) {
+				// 같은 괄호
+				stack.add(strLeft.charAt(i));
+				continue;
+			} else {
+				// 다른 괄호
+				stack.pop();
+				// stack에 있는 것을 없앰
+			}
+		}
 
+		if (stack.size() == 0) {
+			// 올바른 문자열
+			return strLeft + check(strRight);
+		} else {
+			// 올바른 문자열이 아님
+			String fbrmvd = strLeft.substring(1, strLeft.length() - 1);
+			String result = "";
+			for (int i = 0; i < fbrmvd.length(); i++) {
+				if (fbrmvd.charAt(i) == ')') {
+					result += '(';
+				} else {
+					result += ')';
+				}
+			}
+			return "("+check(strRight)+")" + result;
 
+		}
 
-        return answer;
-    }
+	}
 }
