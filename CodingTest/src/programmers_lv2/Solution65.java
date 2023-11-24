3ffd4386c4193532a5170cd6265b2c358c9394c6
Jeeho Kim <rlawlgh3245@gmail.com>
2023-11-25 00:35:11 +0900

    23/11/25

--- CodingTest/src/programmers_lv2/Solution65.java
+++ CodingTest/src/programmers_lv2/Solution65.java
@@ -1,39 +1,56 @@
 package CodingTest.src.programmers_lv2;
 
 public class Solution65 {
-    public int solution(String dirs) {
+	static int[] dc = {0, -1, 0, 1};
+	static int[] dr = {-1, 0, 1, 0};
+	public int solution(String dirs) {
         int answer = 0;
 
-        /**
-         * 방문을 했나 안했나 플래그 처리를 해야함
-         * 같은 좌표를 오게 되면 세지 않음
-         *  */
+        boolean[][][] visited = new boolean[11][11][4]; // 방향까지 
 
-        boolean[][] visited = new boolean[10][10];
-
-        int curXidx = 0;
-        int curYidx = 0;
+        int curC = 5;
+        int curR = 5;
+      
 
         for (int i = 0; i < dirs.length(); i++) {
             char currAlpha = dirs.charAt(i);
+            int d = 0;
             if(currAlpha =='U'){
                 // 위
-                int nextXidx = curXidx;
-                int nextYidx = curYidx + 1;
+            	d = 0;
 
-                if(nextXidx)
+            } else if (currAlpha == 'L') {
+                // 오른쪽
+            	d = 1;
 
             } else if (currAlpha == 'D') {
                 // 아래
-
-            } else if (currAlpha == 'L') {
-                // 왼쪽
+            	d = 2;
 
             } else{
                 // 오른쪽
+            	d = 3;
             }
+            
+            int nextC = curC + dc[d];
+            int nextR = curR + dr[d];
+            
+            if(nextC < 0 || nextR <0 || nextC > 10 || nextR > 10) {
+            	// 범위를 초과하면 
+            	continue;
+            }
+            if(!visited[nextR][nextC][d]) {
+            	// 한번도 지나간적이 없다면
+            	visited[nextR][nextC][d] = true;
+            	// 방문
+            	d = (d%2 == 0 )? 2-d : 4-d;
+            	// 반대쪽도 방문 완료 
+            	visited[curR][curC][d] = true;
+            	answer++; // 방문 체크 
+            }
+            curR = nextR;
+            curC = nextC;
         }
-
         return answer;
     }
 }
