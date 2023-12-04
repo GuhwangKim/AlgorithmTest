223fa59a4a8edbe7562a0f1017cd044b97f1ac07
Jeeho Kim <rlawlgh3245@gmail.com>
2023-12-04 22:26:45 +0900

    23/12/04

--- CodingTest/src/programmers_lv2/Solution68.java
+++ CodingTest/src/programmers_lv2/Solution68.java
@@ -1,40 +1,25 @@
 package CodingTest.src.programmers_lv2;
 
 public class Solution68 {
-    int answer = Integer.MIN_VALUE;
-    public int solution(int[][] triangle) {
-        int level = 0;
-        int sum = 0;
+	public int solution(int[][] triangle) {
+		int answer = Integer.MIN_VALUE;
+		for (int i = 1; i < triangle.length; i++) {
+			// 렬1번쨰 꼭지는 제외하고 더해줌
+			for (int j = 0; j < triangle[i].length; j++) {
+				// 행
+				if (j == 0) { // 왼쪽
+					triangle[i][j] += triangle[i - 1][j]; //
 
-        for (int j = 0; j < triangle.length; j++){
-            // 배열의 위부터 한개씩 빠져나옴
-            for (int i = 0; i < triangle[j].length; i++) {
-                // 현재 배열의 글자 번쨰
-                sum+=triangle[j][i];
-                DFS(i, triangle, level+1, sum);
-                sum-=triangle[j][i];
-            }
-        }
-        return answer;
-    }
+				} else if (j == i) { // 오른쪽
+					triangle[i][j] += triangle[i - 1][j - 1];
+				} else {
+					triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+				}
+				answer = Math.max(answer, triangle[i][j]);
+			}
+		}
 
-    private void DFS(int curIdx, int[][] triangle, int level, int sum) {
-        if(level>= triangle.length){
-            // 마지막 아래까지 온 것
-            answer = Math.max(answer, sum);
-            return;
-        }
+		return answer;
+	}
 
-        // 그 다음 행
-        int[] nextRow = triangle[level];
-
-        for (int i = curIdx; i < 2; i++) {
-            // 그 다음행에서 넘어온 인덱스~ 왼쪽 오른쪽
-            sum+=nextRow[i];
-            DFS(i, triangle, level+1, sum);
-            sum-=nextRow[i];
-        }
-
-
-    }
 }
