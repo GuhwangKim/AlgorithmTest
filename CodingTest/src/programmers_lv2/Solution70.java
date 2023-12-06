f2abce4b2ed3cc57ca7b9975de5ad1b4cc4453e2
JeehoKim <rlawlgh3245@gmail.com>
2023-12-06 13:16:22 +0900

    23/12/06

---
+++ CodingTest/src/programmers_lv2/Solution70.java
@@ -0,0 +1,52 @@
+package CodingTest.src.programmers_lv2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution70 {
+
+    class Order {
+        String direction;
+        String name;
+
+        public Order (String direction, String name){
+            this.direction = direction;
+            this.name = name;
+        }
+    }
+    public String[] solution(String[] record) {
+        String[] answer = {};
+
+        Map<String, Order> map = new HashMap<>();
+
+        for (String rec : record){
+            String[] arr = rec.split(" ");
+            map.put(arr[1], new Order(arr[0], arr[1]));
+        }
+
+        for (int i = 0; i < map.size(); i++) {
+            if(map.get(i).direction.startsWith("E")){
+                // enter
+                answer[i] = map.get(i).name + "´ÔÀÌ ÀÔÀåÇÏ¿´½À´Ï´Ù.";
+                check(map, answer);
+
+            } else if (map.get(i).direction.startsWith("L")) {
+                // leave
+                answer[i] = map.get(i).name + "´ÔÀÌ ³ª°¬½À´Ï´Ù.";
+
+            } else {
+                // change
+
+            }
+        }
+
+
+        return answer;
+    }
+
+    private void check(Map<String, Order> map) {
+
+    }
+}
