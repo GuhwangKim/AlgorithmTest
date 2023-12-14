a70c6ad991fd4e86c076a2668e3f2892ebc9a070
Jeeho Kim <rlawlgh3245@gmail.com>
2023-12-14 22:13:46 +0900

    23/12/14

--- CodingTest/src/programmers_lv2/Solution72.java
+++ CodingTest/src/programmers_lv2/Solution72.java
@@ -1,5 +1,15 @@
 package programmers_lv2;
 
 public class Solution72 {
-
+	
+	 public String[] solution(String[] files) {
+	        String[] answer = {};
+	        // number를 기준으로 나뉨
+	        
+	        for(String file : files) {
+	        	
+	        }
+	        
+	        return answer;
+	    }
 }
