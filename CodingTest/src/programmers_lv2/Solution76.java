6dc7515ac4a781b9138c912de700d9709eb0557c
Jeeho Kim <rlawlgh3245@gmail.com>
2024-01-08 23:01:37 +0900

    24/01/08

--- CodingTest/src/programmers_lv2/Solution76.java
+++ CodingTest/src/programmers_lv2/Solution76.java
@@ -25,7 +25,7 @@ public class Solution76 {
 
     private void dropBlock(int m, int n, char[][] map) {
         // 같은 블럭들이 사라지는 작업
-        for (int c = 0; c < n; c++) { // 행
+        for (int c = 0; c < n; ++c) { // 행
             for (int r = m-1; r >= 0; --r) {
                 // 아래 부터 올라옴
                 if(map[r][c] == '.'){
@@ -49,15 +49,45 @@ public class Solution76 {
     private int checkBlock(int m, int n, char[][] map) {
         // 몇개가 사라질 까 같은 블록을 확인하는 작업
         int cnt = 0;
-        boolean[][] checked = new boolean[][];
+        boolean[][] checked = new boolean[m][n];
 
         for (int i = 0; i < m-1; ++i) {
             for (int j = 0; j < n-1; ++j) {
-
+            	if(map[i][j] == '.') {
+            		// 아무것도 없다면 
+            		continue;
+            	}
+            	checkFour(map, checked, i, j);
             }
         }
-
-
-        return 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+            	if(checked[i][j]) {
+            		// 4면이 같은 것이 존재함 
+            		cnt++;
+            		map[i][j] = '.';
+            		// 없어져야하기 때문에 변경함 
+            	}
+            }
+        }
+        return cnt;
     }
+
+	private void checkFour(char[][] map, boolean[][] checked, int i, int j) {
+		char block = map[i][j];
+		// 현재의 위치 정보 
+		for (int k = i; k < i+2; ++k) {
+			for (int k2 = j; k2 < j+2; ++k2) {
+				if(map[k][k2] != block) {
+					//현재의 위치에서 좌우상하 
+					return;
+				}
+			}
+		}
+		for (int k = i; k < i+2; ++k) {
+			for (int k2 = j; k2 < j+2; ++k2) {
+				checked[k][k2] = true;
+			}
+		}
+	}
 }
