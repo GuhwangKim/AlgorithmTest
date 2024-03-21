e3a79931ec722b9de5df9752104a04ac710f8c56
JeehoKim <rlawlgh3245@gmail.com>
2024-03-21 12:59:53 +0900

    24/03/21

--- CodingTest/src/programmers_lv2/Solution79.java
+++ CodingTest/src/programmers_lv2/Solution79.java
@@ -14,26 +14,26 @@ public class Solution79 {
             queue.add(time);
         }
 
-        int startTime = 9*60; // 가장 빠른 시간
+        int startTime = 9*60; // 처음 버스 출발 시간
         int lastTime = 0;
         int total = 0;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // 버스
             total = 0;
-            while (!queue.isEmpty()){
+            while (!queue.isEmpty()){ // 기다리는 사람 수
                 int currentTime = queue.peek();
                 if(currentTime <= startTime && total < m){
+                    // 시작 시간 보다 일찍왔고, 아직 태울 수 있다면
                     queue.poll();
-                    // 아직 태울 인원 더 있음
                     total++; // 총 태울 인원
                 }else
                     break;
 
-                lastTime = currentTime-1;
+                lastTime = currentTime-1; // 마지막 태운 사람 보다 1초 먼저 와야함
             }
-            startTime+=t;
+            startTime+=t; // 간격을 더함, 그 다음 버스의 시작 시간
         }
-        if(total<m){
+        if(total<m){ // 모든 사람 다 태움
             lastTime = startTime-t;
         }
 
