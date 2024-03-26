bb48122e390275c70105eef290f416a332f25f81
JeehoKim <rlawlgh3245@gmail.com>
2024-03-26 13:15:39 +0900

    24/03/26

---
+++ CodingTest/src/programmers_lv2/Solution81.java
@@ -0,0 +1,9 @@
+package CodingTest.src.programmers_lv2;
+
+public class Solution81 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
