ae77dcf7ce85420fa37c3ea08bf58fc86afdfe6c
JeehoKim <rlawlgh3245@gmail.com>
2024-04-12 13:01:41 +0900

    24/04/12 - 일반 풀이

--- CodingTest/src/programmers_lv2/Solution83.java
+++ CodingTest/src/programmers_lv2/Solution83.java
@@ -5,37 +5,18 @@ import java.util.Arrays;
 public class Solution83 {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int restApt = 0;
-        int infldApt = 0;
+        int index = 0; // 기지국 인덱스
+        int aptIndx = 1; // 총 아파트
 
-        for (int station : stations){
-            if(station + w < n || station - w < 0){
-                // 전파가 오른쪽으로 더 많이 감
-                if(station + w < n){
-                    infldApt = (n-station);
-
-                // 전파가 왼쪽으로 더 많이 감
-                }else{
-                    infldApt = station-1;
-                }
-            } else {
-                infldApt += w * 2;
+        while (aptIndx <=n) {
+            if (index < stations.length && aptIndx >= stations[index] - w) {
+                aptIndx = stations[index]+w+1;
+                aptIndx++;
+            } else{
+                answer += 1;
+                aptIndx += (w*2)+1;
             }
-            infldApt++; //본인것
         }
-
-        restApt = n-infldApt;
-
-        if(restApt==0){
-            return answer;
-        }
-
-        // 이미 표시된 부분 말고 다른 부분에 값을 주고, 계속해서 같은 함수를 돌림
-
-
-
-
-
 
         return answer;
     }
