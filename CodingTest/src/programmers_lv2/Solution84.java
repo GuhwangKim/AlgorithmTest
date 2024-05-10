8d6c0049b6a218a9410836b52c41067078967041
JeehoKim <rlawlgh3245@gmail.com>
2024-05-10 13:08:24 +0900

    24/05/10

--- CodingTest/src/programmers_lv2/Solution84.java
+++ CodingTest/src/programmers_lv2/Solution84.java
@@ -4,27 +4,29 @@ package CodingTest.src.programmers_lv2;
  * dijkstra
  * */
 public class Solution84 {
-    static int cnt;
-    static int[][] arr;
-    static int[] dist;
-    static boolean[] visited;
+    static final int INF = 500001;
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        cnt = 0;
-        arr = new int[N + 1][N + 1];
-        // 인접행렬초기화
-        for (int i = 0; i < N + 1; i++) {
-            for (int j = 0; j < N + 1; j++) {
-                arr[i][j] = 500001;
+        int[][] map = new int[N + 1][N + 1];
+
+        // 무한대로 초기화
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                map[i][j] = INF;
             }
         }
-        // 인접행렬 거리 값
+
+        // 간선 정보 저장
         for (int i = 0; i < road.length; i++) {
-            if(arr[road[i][0]][road[i][1]] > road[i][2]){
+            int a = road[i][0];
+            int b = road[i][1];
+            int w = road[i][2];
 
-            }
+
         }
-
 
         return answer;
     }
