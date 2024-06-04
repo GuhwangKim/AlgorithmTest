7ab268610dec93b587320b6c44d6ec85ed6e036d
JeehoKim <rlawlgh3245@gmail.com>
2024-06-04 13:07:01 +0900

    24/06/04

--- CodingTest/src/programmers_lv2/Solution85.java
+++ CodingTest/src/programmers_lv2/Solution85.java
@@ -2,30 +2,32 @@ package CodingTest.src.programmers_lv2;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Solution85 {
     // 이어져있는 문자열 찾기
     public int solution(String s) {
         int answer = -1;
 
-        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if (!queue.isEmpty()) {
-                if (queue.peek() == s.charAt(i)) {
-                    queue.poll();
+            if (!stack.isEmpty()) {
+                if (stack.peek() == s.charAt(i)) {
+                    stack.pop();
                 } else{
-                    queue.add(s.charAt(i));
+                    stack.add(s.charAt(i));
                 }
+            }else{
+                stack.add(s.charAt(i));
             }
         }
 
-        if (queue.isEmpty()) {
+        if (stack.isEmpty()) {
             answer = 1;
         }else{
             answer = 0;
         }
-
         return answer;
     }
 }
---
+++ CodingTest/src/programmers_lv2/Solution85Test.java
@@ -0,0 +1,9 @@
+package CodingTest.src.programmers_lv2;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution85Test {
+
+}
