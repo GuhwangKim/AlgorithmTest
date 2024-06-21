44f2d7ad796b1db5c9391fbdb3c099b7b78b0f59
Jeeho Kim <rlawlgh3245@gmail.com>
2024-06-21 20:59:26 +0900

    24/06/21

--- CodingTest/src/programmers_lv2/Solution88.java
+++ CodingTest/src/programmers_lv2/Solution88.java
@@ -8,24 +8,32 @@ import java.util.Queue;
 public class Solution88 {
     public int solution(int[] arr) {
         int answer = 0;
-        // 최대공약수 구함
-        Queue<Integer> queue1 = new LinkedList<>();
-        Arrays.stream(arr).forEach(value -> queue1.add(value));
-
-
-
+        // 최대공약수 구함 
+//        Queue<Integer> queue = new LinkedList<Integer>();
+//        Arrays.stream(arr).filter(value -> queue.add(value));
+//        while (queue.size()>1) {
+//			queue.add(lcm(queue.poll(), queue.poll()));
+//		}
+        // 해당 방법 너무 번거로워서 재귀함수로 
+        
+        int gcd = getGCD(arr[0], arr[1]);
+        int lcm = (arr[0]*arr[1])/gcd;
+        
+        for (int i = 2; i < arr.length; i++) {
+			gcd = getGCD(lcm, arr[i]);
+			lcm = (lcm * arr[i]) / gcd;
+		}
+        
         // 모든 숫자를 곱한 값 / 최대공약수
 
-        return answer;
+        return lcm;
+    }
+    
+    public int getGCD (int a, int b) {
+    	if(a%b==0) {
+    		return b;
+    	}
+    	return getGCD(b, a%b);
     }
 
-    public int lcm(int a, int b) {
-        int c = 0;
-        while (b != 0) {
-            c = a%b;
-            a = b ;
-            b= c;
-        }
-        return a;
-    }
 }
