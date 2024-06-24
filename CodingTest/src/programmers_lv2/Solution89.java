17cc3f47d92b4f3f04e97c66a04695aa2e44edc9
JeehoKim <rlawlgh3245@gmail.com>
2024-06-24 13:10:50 +0900

    24/06/24

---
+++ CodingTest/src/programmers_lv2/Solution89.java
@@ -0,0 +1,18 @@
+package CodingTest.src.programmers_lv2;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Solution89 {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] strArr = s.split(" ");
+
+        Arrays.stream(strArr).map(s1 -> Character.toUpperCase(s1.charAt(0)) + s1.substring(1, s1.length()).toLowerCase()).collect(Collectors.joining(" "));
+
+
+
+        return answer;
+    }
+}
