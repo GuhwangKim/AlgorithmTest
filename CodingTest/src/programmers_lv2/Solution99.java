85f1153434025ad2fffd11b845a7270872fa0a1e
JeehoKim <rlawlgh3245@gmail.com>
2024-08-08 13:10:49 +0900

    24/08/08

---
+++ CodingTest/src/programmers_lv2/Solution99.java
@@ -0,0 +1,14 @@
+package CodingTest.src.programmers_lv2;
+
+public class Solution99 {
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        // 한줄로 새우려고 함
+        // 만약 거기서 못낀 수가 하나라도 있으면 일단 순서는 제대로 알 수 없음
+
+        // 뭔가 동적으로 빼고 그런거였음 좋겠는데
+
+
+        return answer;
+    }
+}
