9b0f44c4d7e45a0872c99356e055cc2db3fe7036
JeehoKim <rlawlgh3245@gmail.com>
2024-08-09 13:20:09 +0900

    24/08/09

--- CodingTest/src/programmers_lv2/Solution99.java
+++ CodingTest/src/programmers_lv2/Solution99.java
@@ -3,11 +3,27 @@ package CodingTest.src.programmers_lv2;
 public class Solution99 {
     public int solution(int n, int[][] results) {
         int answer = 0;
-        // 한줄로 새우려고 함
-        // 만약 거기서 못낀 수가 하나라도 있으면 일단 순서는 제대로 알 수 없음
 
-        // 뭔가 동적으로 빼고 그런거였음 좋겠는데
+        // 모든 사람과의 대항전을 표시할 수 있는 그래프
+        int[][] graph = new int[n + 1][n + 1];
+        // 배열에 들어간 숫자는 1부터 이므로
 
+        for (int[] edge : results) {
+            graph[edge[0]][edge[1]] = 1;
+            // 이긴 표시
+            graph[edge[1]][edge[0]] = -1;
+            // 진 표시
+        }
+
+        for (int i = 1; i <= n; i++) {
+            // 도착
+            for (int j = 1; j <= n; j++) {
+                // 거쳐감
+                for (int k = 1; k <= n; k++) {
+
+                }
+            }
+        }
 
         return answer;
     }
