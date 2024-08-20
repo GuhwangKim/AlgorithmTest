6078d80676f03f407366fe3fafc007d9dd4d1e3c
JeehoKim <rlawlgh3245@gmail.com>
2024-08-20 13:15:37 +0900

    24/08/20

--- CodingTest/src/programmers_lv2/Solution101.java
+++ CodingTest/src/programmers_lv2/Solution101.java
@@ -1,21 +1,33 @@
 package CodingTest.src.programmers_lv2;
 
 public class Solution101 {
+    int n;
+    int[][] directions;
+    boolean[][] visited;
+    int K2;
+    int answer;
     public int solution(int N, int[][] road, int K) {
-        int answer = 0;
+        answer = 0;
+        n = N;
+        K2 = K;
         // 각각 모든 조합을 넣을 수 있는 배열 생성
-        int[][] directions = new int[N+1][N+1];
+        directions = new int[N+1][N+1];
+        visited = new boolean[N + 1][N + 1];
         for (int[] each : road) {
             directions[each[0]][each[1]] = each[2];
             directions[each[1]][each[2]] = each[2];
         }
 
         for (int i = 1; i <= N ; i++) { // level
-            for (int j = 1; j <= N; j++) { // row
+            for (int j = i+1; j <= N; j++) { // row
                 if (directions[i][j] == 0) {
                     continue; // 값이 없으면 건너띄고
                 }else{
-                    DFS(i, j, directions[i][j]);
+                    visited[i][j] = true;
+                    visited[j][i] = true;
+                    if (directions[i][j] <= K) {
+                        DFS(i, j, directions[i][j]);
+                    }
                 }
             }
         }
@@ -25,6 +37,20 @@ public class Solution101 {
     }
 
     private void DFS(int row, int col, int total) {
+        for (int i = col; i <=n ; i++) {
+            for (int j = row; j <=n ; j++) {
+                if (!visited[i][j] && directions[i][j] != 0) {
+                    // 방문하지 않았고, 값이 있다
+                    total += directions[i][j];
+                    if (total >= K2) {
+                        answer++;
+                        return;
+                    }else{
+                        DFS(i, j, total);
+                    }
+                }
+            }
 
+        }
     }
 }
