f09cb400c88fa4c7883f5d6afa891d7a4455b7ce
JeehoKim <rlawlgh3245@gmail.com>
2024-08-26 13:26:57 +0900

    24/08/26

---
+++ CodingTest/src/programmers_lv2/Solution102.java
@@ -0,0 +1,17 @@
+package CodingTest.src.programmers_lv2;
+
+/**
+ * 멀리 뛰기 https://school.programmers.co.kr/learn/courses/30/lessons/12914
+ * **/
+public class Solution102 {
+    public long solution(int n) {
+        long answer = 0;
+        // 1칸 또는 2칸 가능
+        // 1) 모든 것이 다 1일 때 (최대)
+        // 그 모든 경우의 수를 다 더하면 되는ㄷ
+        // => 여기서 경우의 수를 어떻게..?
+        // 2) 2로 나누어서 몫 + 나머지 일때 (최소)
+
+        return answer;
+    }
+}
