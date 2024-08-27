5e79d26f34534356d38cee3d9c177f8d456bfa1f
JeehoKim <rlawlgh3245@gmail.com>
2024-08-27 13:00:12 +0900

    24/08/27

--- CodingTest/src/programmers_lv2/Solution102.java
+++ CodingTest/src/programmers_lv2/Solution102.java
@@ -5,13 +5,12 @@ package CodingTest.src.programmers_lv2;
  * **/
 public class Solution102 {
     public long solution(int n) {
-        long answer = 0;
-        // 1칸 또는 2칸 가능
-        // 1) 모든 것이 다 1일 때 (최대)
-        // 그 모든 경우의 수를 다 더하면 되는ㄷ
-        // => 여기서 경우의 수를 어떻게..?
-        // 2) 2로 나누어서 몫 + 나머지 일때 (최소)
-
-        return answer;
+        long[] answer = new long[2001];
+        answer[1] = 1;
+        answer[2] = 2;
+        for (int i = 3; i < 2001; i++) {
+            answer[i] = (answer[i-1]+answer[i-2])%1234567;
+        }
+        return answer[n];
     }
 }
